5ce442c7077c4f56f46c130a449066454ea36427
Charles Alleman <charles@businessinmind.com.au>
2019-07-08 16:24:19 +1000

    Formatted Entire Document

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from "@angular/core"
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    NgbModule,
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [NgbModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
