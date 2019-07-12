e1b5e68dede77228967ed612d8f0861fbb9b6e73
Charles Alleman <charles@alleman.com.au>
2019-07-12 17:54:41 +1000

    Updated test browsers
    Also added a small optimisation to cards component.

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,9 @@ module.exports = function(config) {
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
+      require("karma-edge-launcher"),
       require("karma-chrome-launcher"),
+      require("karma-firefox-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage-istanbul-reporter"),
       require("@angular-devkit/build-angular/plugins/karma")
@@ -25,7 +27,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["Edge", "Chrome", "Firefox"],
     singleRun: false,
     restartOnFileChange: true
   });
--- src/app/component/shared/cards/cards.component.ts
+++ src/app/component/shared/cards/cards.component.ts
@@ -26,21 +26,16 @@ export class CardsComponent implements OnInit {
     this.checkRequiredFields('cards', this.cards);
 
     this.imageCards = false;
-    let noImageCards = false;
     this.cards.forEach(card => {
       if (card.image) {
         this.imageCards = true;
-      } else {
-        noImageCards = true;
+      } else if (this.imageCards) {
+        // If some cards have images but others do not, throw error
+        throw new Error(
+          'If an image is given, all cards must have image component.'
+        );
       }
     });
-
-    // If some cards have images but others do not, throw error
-    if (this.imageCards && noImageCards) {
-      throw new Error(
-        'If an image is given, all cards must have image component.'
-      );
-    }
   }
 
   checkRequiredFields(name: string, input: any) {
