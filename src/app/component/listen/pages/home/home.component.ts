07e94004cabbba76314e52c4126fe78f7e0ccc0a
Charles Alleman <charles@alleman.com.au>
2019-07-19 16:56:06 +1000

    Broken annotirous when component destroyed

--- src/app/component/listen/pages/home/home.component.ts
+++ src/app/component/listen/pages/home/home.component.ts
@@ -1,13 +1,31 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-listen',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListenComponent implements OnInit {
+export class ListenComponent implements OnInit, OnDestroy {
+  imageUrl: string;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.debug('Create');
+    this.imageUrl =
+      'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fc%2Fc5%2FSpectrogram-19thC.png&f=1';
+  }
+
+  ngOnDestroy() {
+    console.debug('Destroy');
+    this.imageUrl = null;
+  }
 }
