8337e7361bc48a07f7445da20a8693d1178280af
Charles Alleman <charles@alleman.com.au>
2019-07-24 22:34:45 +1000

    Created layout menu service

--- src/app/component/projects/pages/home/home.component.ts
+++ src/app/component/projects/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { List } from 'immutable';
 import { Card } from 'src/app/component/shared/cards/cards.component';
 import { BawApiService } from 'src/app/services/baw-api/baw-api.service';
+import { MenusService } from './menus.service';
 
 @Component({
   selector: 'app-projects',
@@ -26,7 +27,7 @@ export class ProjectsComponent implements OnInit {
     label: string;
   }[];
 
-  constructor(private api: BawApiService) {}
+  constructor(private api: BawApiService, private menus: MenusService) {}
 
   ngOnInit() {
     this.projectList = List([
@@ -124,83 +125,8 @@ export class ProjectsComponent implements OnInit {
         link: 'https://www.ecosounds.org/projects/1029'
       }
     ]);
-    this.secondaryLinks = [
-      {
-        route: '/',
-        icon: ['fas', 'home'],
-        label: 'Home',
-        tooltip: 'Home page'
-      },
-      {
-        route: '/login',
-        icon: ['fas', 'sign-in-alt'],
-        label: 'Log in',
-        tooltip: 'View profile'
-      },
-      {
-        route: '/register',
-        icon: ['fas', 'user-plus'],
-        label: 'Register',
-        tooltip: 'View my recent annotations'
-      },
-      {
-        route: '/projects',
-        icon: ['fas', 'globe-asia'],
-        label: 'Projects',
-        tooltip: 'View projects I have access too'
-      },
-      {
-        route: '/audio_analysis',
-        icon: ['fas', 'server'],
-        label: 'Audio Analysis',
-        tooltip: 'View audio analysis jobs'
-      },
-      {
-        route: '/library',
-        icon: ['fas', 'book'],
-        label: 'Library',
-        tooltip: 'Annotation library'
-      },
-      {
-        route: '/data_request',
-        icon: ['fas', 'table'],
-        label: 'Data Request',
-        tooltip: 'Request customized data from the website'
-      },
-      {
-        route: '/send_audio',
-        icon: ['fas', 'envelope'],
-        label: 'Send Audio',
-        tooltip: 'Send us audio recordings to upload'
-      },
-      {
-        route: '/about/report',
-        icon: ['fas', 'bug'],
-        label: 'Report Problem',
-        tooltip: 'Report a problem with the website'
-      },
-      {
-        route: '/statistics',
-        icon: ['fas', 'chart-line'],
-        label: 'Statistics',
-        tooltip: 'Annotation and audio recording stats'
-      }
-    ];
-    this.actionTitle = { icon: ['fas', 'globe-asia'], label: 'Projects' };
-
-    if (this.api.loggedIn) {
-      this.actionLinks = [
-        {
-          route: 'https://www.ecosounds.org/projects/new',
-          icon: ['fas', 'plus'],
-          label: 'New project'
-        },
-        {
-          route: 'https://www.ecosounds.org/projects/new_access_request',
-          icon: ['fas', 'key'],
-          label: 'Request access'
-        }
-      ];
-    }
+    this.secondaryLinks = this.menus.secondaryMenu();
+    this.actionTitle = this.menus.actionTitle();
+    this.actionLinks = this.menus.actionLinks();
   }
 }
---
+++ src/app/services/layout-menus/layout-menus.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { BawApiService } from '../baw-api/baw-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutMenusService {
+  constructor(
+    private _api: BawApiService,
+    private _secondaryMenuJson: any,
+    private _actionLinksJson: any
+  ) {}
+
+  /**
+   * Returns the secondary menu links
+   * @returns List of secondary links
+   */
+  secondaryMenu(): SecondaryLink[] {
+    if (this._api.loggedIn) {
+      return this._secondaryMenuJson.auth.map(function(
+        link: SecondaryLink
+      ): SecondaryLink {
+        return link;
+      });
+    } else {
+      return this._secondaryMenuJson.no_auth.map(function(
+        link: SecondaryLink
+      ): SecondaryLink {
+        return link;
+      });
+    }
+  }
+
+  /**
+   * Returns the action title
+   * @returns Action title and icon
+   */
+  actionTitle(): ActionTitle {
+    return {
+      label: this._actionLinksJson.title.label,
+      icon: [
+        this._actionLinksJson.title.icon.style,
+        this._actionLinksJson.title.icon.glyph
+      ]
+    };
+  }
+
+  /**
+   * Returns the action menu links
+   * @returns List of action links
+   */
+  actionLinks(): ActionLink[] {
+    if (this._api.loggedIn) {
+      return this._actionLinksJson.auth.map(function(
+        link: ActionLink
+      ): ActionLink {
+        return link;
+      });
+    } else {
+      return this._actionLinksJson.no_auth.map(function(
+        link: ActionLink
+      ): ActionLink {
+        return link;
+      });
+    }
+  }
+}
+
+export interface SecondaryLink {
+  route: string;
+  icon: [string, string];
+  label: string;
+  tooltip: string;
+}
+
+export interface ActionLink {
+  route: string;
+  icon: [string, string];
+  label: string;
+}
+export interface ActionTitle {
+  icon: [string, string];
+  label: string;
+}
