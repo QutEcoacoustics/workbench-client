05ad76f0e6aac0fae65754b25e88a19b5c7ca91c
Charles Alleman <charles@alleman.com.au>
2020-01-06 17:12:48 +1000

    Added uri support for item component

--- src/app/component/shared/items/item/item.component.ts
+++ src/app/component/shared/items/item/item.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from "@angular/core";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { Href } from "src/app/interfaces/menusInterfaces";
 import { StrongRoute } from "src/app/interfaces/strongRoute";
@@ -7,10 +12,32 @@ import { StrongRoute } from "src/app/interfaces/strongRoute";
   selector: "app-items-item",
   template: `
     <div class="clearfix" style="font-size: 0.925rem;">
+      <!-- Item icon -->
       <fa-icon id="icon" [icon]="icon"></fa-icon>
+
+      <!-- Item name -->
       <span id="name">
-        {{ name }}
+        <ng-container *ngIf="uri; else plainText">
+          <ng-container *ngIf="isInternalRoute(); else externalLink">
+            <!-- URI is internal link -->
+            <a [routerLink]="uri.toString()">
+              {{ name }}
+            </a>
+          </ng-container>
+          <ng-template #externalLink>
+            <!-- URI is external link -->
+            <a [href]="uri">
+              {{ name }}
+            </a>
+          </ng-template>
+        </ng-container>
+        <ng-template #plainText>
+          <!-- No URI -->
+          {{ name }}
+        </ng-template>
       </span>
+
+      <!-- Item value -->
       <span id="value" class="badge badge-pill badge-secondary float-right">
         {{ value }}
       </span>
@@ -22,9 +49,13 @@ export class ItemComponent {
   @Input() icon: IconProp;
   @Input() name: string;
   @Input() value: string | number;
-  @Input() uri: Href | StrongRoute;
+  @Input() uri?: Href | StrongRoute;
 
   constructor() {}
+
+  isInternalRoute() {
+    return typeof this.uri === "object";
+  }
 }
 
 export interface ItemInterface {
