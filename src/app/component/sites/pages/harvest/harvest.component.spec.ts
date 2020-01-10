5fc4ac50d015e85449b5525f5ebf6b411347b724
Charles Alleman <charles@alleman.com.au>
2020-01-10 13:08:05 +1000

    Added unit tests

--- src/app/component/sites/pages/harvest/harvest.component.spec.ts
+++ src/app/component/sites/pages/harvest/harvest.component.spec.ts
@@ -1,23 +1,62 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { testAppInitializer } from "src/app/app.helper";
+import { SharedModule } from "src/app/component/shared/shared.module";
+import { AppConfigService } from "src/app/services/app-config/app-config.service";
 import { HarvestComponent } from "./harvest.component";
 
-describe("HarvestComponent", () => {
+describe("SiteHarvestComponent", () => {
+  let httpMock: HttpTestingController;
+  let config: AppConfigService;
   let component: HarvestComponent;
   let fixture: ComponentFixture<HarvestComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HarvestComponent]
+      imports: [SharedModule, HttpClientTestingModule],
+      declarations: [HarvestComponent],
+      providers: [...testAppInitializer]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HarvestComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.get(HttpTestingController);
+    config = TestBed.get(AppConfigService);
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+
   it("should create", () => {
+    httpMock.expectOne(
+      config.getConfig().environment.cmsRoot + "/harvest.html"
+    );
     expect(component).toBeTruthy();
+  });
+
+  it("should load cms", () => {
+    const req = httpMock.expectOne(
+      config.getConfig().environment.cmsRoot + "/harvest.html"
+    );
+
+    req.flush(
+      "<h1>Test Header</h1><p class='description'>Test Description</p>"
+    );
+    fixture.detectChanges();
+
+    const header = fixture.nativeElement.querySelector("h1");
+    const body = fixture.nativeElement.querySelector("p.description");
+
+    expect(header).toBeTruthy();
+    expect(header.innerText.trim()).toBe("Test Header");
+    expect(body).toBeTruthy();
+    expect(body.innerText.trim()).toBe("Test Description");
   });
 });
