cbcb91ba8c1f1b0f63a6324d05f4f21a62b7135e
Charles Alleman <charles@alleman.com.au>
2020-02-03 09:16:06 +1000

    Fixed getTest test helper function

--- src/testHelpers.ts
+++ src/testHelpers.ts
@@ -3,6 +3,6 @@ import { By } from "@angular/platform-browser";
 
 export function getText(target: DebugElement, selector: string) {
   return target
-    .queryAll(By.css("app-menu-internal-link span"))
+    .queryAll(By.css(selector))
     .map(x => x.nativeElement.textContent.trim());
 }
