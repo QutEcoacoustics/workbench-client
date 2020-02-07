899c1db19c0640529f22d416aa2dcbca16497f80
Charles Alleman <charles@alleman.com.au>
2020-02-07 16:32:09 +1000

    Created EnvironmentBuilder

---
+++ tools/EnvironmentBuilder.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const environmentPath = "./dist/environment";
+
+buildEnvironment("staging");
+buildEnvironment("production");
+
+function buildEnvironment(stage) {
+  path = `${environmentPath}.${stage}.json`;
+  environment = { appConfig: `assets/config/${stage}.json` };
+
+  try {
+    const stat = fs.statSync(path);
+
+    if (stat.isFile()) {
+      fs.unlinkSync(path);
+    }
+  } catch (err) {}
+
+  fs.writeFile(path, JSON.stringify(environment), err => {
+    if (err) {
+      return console.error(`Environment Builder (${stage}): `, err);
+    }
+
+    console.log(`Environment Builder (${stage}): Success`);
+  });
+}
