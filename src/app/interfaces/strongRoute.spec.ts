2e977dc9d3dc25cb9f869004ac293716bb4145cf
Charles Alleman <charles@alleman.com.au>
2020-02-18 17:35:34 +1000

    StrongRoute unit test placeholders

---
+++ src/app/interfaces/strongRoute.spec.ts
@@ -0,0 +1,58 @@
+describe("StrongRoute", () => {
+  describe("Base StrongRoute", () => {
+    it("should create base route", () => {});
+    it("should add route", () => {});
+    it("should add multiple routes", () => {});
+    it("should add parameter route", () => {});
+    it("should add mixed routes", () => {});
+  });
+
+  describe("Child StrongRoute", () => {
+    it("should add route", () => {});
+    it("should add multiple routes", () => {});
+    it("should add parameter route", () => {});
+    it("should add mixed routes", () => {});
+  });
+
+  describe("Sorting", () => {
+    it("should order '\"\"' routes first", () => {});
+    it("should order parameter route last", () => {});
+  });
+
+  describe("Formatting", () => {
+    it("should format base route", () => {});
+    it("should format child route", () => {});
+    it("should format parameter route", () => {});
+    it("should format child parameter route", () => {});
+  });
+
+  describe("Compile Routes", () => {
+    it("should compile base route with route", () => {});
+    it("should compile base route with parameter route", () => {});
+    it("should compile base route with mixed routes", () => {});
+    it("should compile child StrongRoute with route", () => {});
+    it("should compile child StrongRoute with parameter route", () => {});
+    it("should compile child StrongRoute with mixed routes", () => {});
+  });
+
+  describe("toString", () => {
+    it("should handle base StrongRoute", () => {});
+    it("should handle child StrongRoute", () => {});
+    it("should handle grandchild StrongRoute", () => {});
+    it("should handle parameter StrongRoute", () => {});
+  });
+
+  describe("toRoute", () => {
+    it("should handle base StrongRoute", () => {});
+    it("should handle child StrongRoute", () => {});
+    it("should handle grandchild StrongRoute", () => {});
+    it("should handle parameter StrongRoute", () => {});
+  });
+
+  describe("routeConfig", () => {
+    it("should handle base StrongRoute", () => {});
+    it("should handle child StrongRoute", () => {});
+    it("should handle grandchild StrongRoute", () => {});
+    it("should handle parameter StrongRoute", () => {});
+  });
+});
