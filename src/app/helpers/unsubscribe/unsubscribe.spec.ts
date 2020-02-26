63b77b0cef554f1f775e98239eb9772b018058df
Charles Alleman <charles@alleman.com.au>
2020-02-26 17:33:49 +1000

    Defined WithUnsubscribe test headers

---
+++ src/app/helpers/unsubscribe/unsubscribe.spec.ts
@@ -0,0 +1,7 @@
+describe("WithUnsubscribe", () => {
+  it("should accept empty base class", () => {});
+  it("should accept PageComponent base class", () => {});
+  it("should provide unsubscribe observable", () => {});
+  it("should call next() when ngOnDestroy is called", () => {});
+  it("should call complete() when ngOnDestroy is called", () => {});
+});
