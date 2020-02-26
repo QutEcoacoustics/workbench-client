340eb1c6439ec66bf1a7e7d09d05e741f0244f1e
Charles Alleman <charles@alleman.com.au>
2020-02-26 17:52:14 +1000

    Implemented unit tests

--- src/app/helpers/unsubscribe/unsubscribe.spec.ts
+++ src/app/helpers/unsubscribe/unsubscribe.spec.ts
@@ -1,7 +1,56 @@
+import { WithUnsubscribe } from "./unsubscribe";
+import { Subject } from "rxjs";
+
 describe("WithUnsubscribe", () => {
-  it("should accept empty base class", () => {});
-  it("should accept PageComponent base class", () => {});
-  it("should provide unsubscribe observable", () => {});
-  it("should call next() when ngOnDestroy is called", () => {});
-  it("should call complete() when ngOnDestroy is called", () => {});
+  it("should accept empty base class", () => {
+    class MockClass extends WithUnsubscribe() {
+      public test = "MockClass";
+    }
+
+    const mockClass = new MockClass();
+
+    expect(mockClass).toBeTruthy();
+    expect(mockClass.test).toBe("MockClass");
+  });
+
+  it("should accept custom class base class", () => {
+    class MockBaseClass {
+      public base = "MockBaseClass";
+    }
+    class MockClass extends WithUnsubscribe(MockBaseClass) {
+      public test = "Custom Test";
+    }
+
+    const mockClass = new MockClass();
+
+    expect(mockClass).toBeTruthy();
+    expect(mockClass.base).toBe("MockBaseClass");
+    expect(mockClass.test).toBe("Custom Test");
+  });
+
+  it("should provide unsubscribe observable", () => {
+    class MockClass extends WithUnsubscribe() {}
+    const mockClass = new MockClass();
+
+    expect(mockClass["unsubscribe"]).toBeTruthy();
+    expect(mockClass["unsubscribe"] instanceof Subject).toBeTrue();
+  });
+
+  it("should call next() when ngOnDestroy is called", () => {
+    class MockClass extends WithUnsubscribe() {}
+    const mockClass = new MockClass();
+    const spy = spyOn(mockClass["unsubscribe"], "next").and.callThrough();
+
+    mockClass.ngOnDestroy();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("should call complete() when ngOnDestroy is called", () => {
+    class MockClass extends WithUnsubscribe() {}
+    const mockClass = new MockClass();
+    const spy = spyOn(mockClass["unsubscribe"], "complete").and.callThrough();
+
+    mockClass.ngOnDestroy();
+    expect(spy).toHaveBeenCalled();
+  });
 });
