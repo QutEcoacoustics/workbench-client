111317cd0beb768ad689d28033bfe132fc5a57f7
Charles Alleman <charles@businessinmind.com.au>
2020-02-28 14:10:25 +1000

    Created DatatableDirective

--- src/app/component/shared/shared.components.ts
+++ src/app/component/shared/shared.components.ts
@@ -8,7 +8,7 @@ import { FormlyBootstrapModule } from "@ngx-formly/bootstrap";
 import { FormlyModule } from "@ngx-formly/core";
 import { LoadingBarHttpClientModule } from "@ngx-loading-bar/http-client";
 import { NgxDatatableModule } from "@swimlane/ngx-datatable";
-import { ToastrModule } from "ngx-toastr";
+import { DatatableDirective } from "src/app/directives/datatable/datatable.directive";
 import { ActionMenuComponent } from "./action-menu/action-menu.component";
 import { CardsModule } from "./cards/cards.module";
 import { CmsComponent } from "./cms/cms.component";
@@ -41,7 +41,8 @@ export const sharedComponents = [
   FormlyQuestionAnswer,
   FormlyQuestionAnswerAction,
   TimezoneFormPipe,
-  LoadingComponent
+  LoadingComponent,
+  DatatableDirective
 ];
 
 export const sharedModules = [
