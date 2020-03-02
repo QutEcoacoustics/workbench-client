c10da5c4909723c5b52dc1c3669d287854c86387
Charles Alleman <charles@alleman.com.au>
2020-03-02 10:52:12 +1000

    Created DatatableDirective unit tests

--- src/app/directives/datatable/datatable.directive.spec.ts
+++ src/app/directives/datatable/datatable.directive.spec.ts
@@ -1,23 +1,86 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { DatatableDirective } from "./datatable.directive";
+import { Component, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { SharedModule } from "src/app/component/shared/shared.module";
 
-describe("DefaultDatatableComponent", () => {
-  let component: DatatableDirective;
-  let fixture: ComponentFixture<DatatableDirective>;
+describe("DatatableDirective", () => {
+  let component: MockComponent;
+  let fixture: ComponentFixture<MockComponent>;
+  let datatable: DebugElement;
+
+  @Component({
+    template: `
+      <ngx-datatable
+        #table
+        appDatatable
+        [datatable]="table"
+        [rows]="rows"
+        [columns]="columns"
+      >
+      </ngx-datatable>
+    `
+  })
+  class MockComponent {
+    rows = [{ id: 1 }];
+    columns = [{ prop: "id" }];
+  }
+
+  function assertAttribute(
+    selector: string,
+    attribute: string,
+    expectedValue: string
+  ) {
+    const target = datatable.query(By.css(selector));
+    expect(target).toBeTruthy();
+    expect(target.attributes[attribute]).toBeTruthy();
+    expect(target.attributes[attribute]).toBe(expectedValue);
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DatatableDirective]
+      declarations: [MockComponent, DatatableDirective],
+      imports: [SharedModule]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DatatableDirective);
+    fixture = TestBed.createComponent(MockComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    datatable = fixture.debugElement.query(By.css("ngx-datatable"));
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should set class", () => {
+    expect(Object.keys(datatable.classes)).toContain("bootstrap");
+  });
+
+  it("should set footer height", () => {
+    assertAttribute("datatable-footer", "ng-reflect-footer-height", "50");
+  });
+
+  it("should set header height", () => {
+    assertAttribute("datatable-header", "ng-reflect-header-height", "50");
+  });
+
+  it("should set limit", () => {
+    assertAttribute("datatable-body", "ng-reflect-page-size", "25");
+  });
+
+  it("should set row height", () => {
+    assertAttribute("datatable-body", "ng-reflect-row-height", "auto");
+  });
+
+  it("should set horizontal scroll bar", () => {
+    assertAttribute("datatable-scroller", "ng-reflect-scrollbar-h", "true");
+  });
+
+  it("should set not reorderable", () => {
+    assertAttribute("datatable-header", "ng-reflect-reorderable", "false");
   });
 });
