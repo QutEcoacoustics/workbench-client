3c06a6dd5105459ff3f066d1e6cff88236531b7a
Charles Alleman <charles@alleman.com.au>
2020-04-04 14:07:02 +1000

    Prevent case-converter from changing '.'
    - Prevented caseConverter function from modifying keys which included '.'
    - Updated test suite to 100% coverage
    - Fixed whitelisted value conversions (was previously not working for arrays and objects)

--- src/app/helpers/case-converter/case-converter.ts
+++ src/app/helpers/case-converter/case-converter.ts
@@ -1,21 +1,32 @@
 import camelCase from "lodash.camelcase";
 import snakeCase from "lodash.snakecase";
+
+// List of whitelist keys which should have their values converted
 import * as whitelist from "./whitelist.json";
 
 /**
  * Deeply converts keys of an object from one case to another.
- * Function was created here: https://github.com/travelperk/case-converter
+ * Function was created here: https://github.com/travelperk/case-converter (repo no longer exists)
  * @copyright MIT
+ * @author travelperk
  * @param oldObject to convert
- * @param converterFunction to convert key.
+ * @param callback to convert key.
  * @return converted object
  */
 const convertCase = (
   oldObject: any,
-  converterFunction: (string?: string) => string,
+  callback: (string?: string) => string,
   convertValue?: boolean
 ): any => {
   let newObject: any;
+
+  // Prevent conversion of periods (.)
+  function converterFn(string?: string): string {
+    return string
+      .split(".")
+      .map((split) => callback(split))
+      .join(".");
+  }
 
   if (
     !oldObject ||
@@ -23,32 +34,24 @@ const convertCase = (
     !Object.keys(oldObject).length
   ) {
     // Change object value
-    return convertValue ? converterFunction(oldObject) : oldObject;
+    return convertValue ? converterFn(oldObject) : oldObject;
   }
 
   if (Array.isArray(oldObject)) {
-    newObject = oldObject.map(element =>
-      convertCase(element, converterFunction)
+    newObject = oldObject.map((element) =>
+      convertCase(element, callback, convertValue)
     );
   } else {
     newObject = {};
 
     // Change object keys
-    Object.keys(oldObject).forEach(oldKey => {
-      const newKey = converterFunction(oldKey);
-      let whitelisted = false;
+    Object.keys(oldObject).forEach((oldKey) => {
+      const newKey = converterFn(oldKey);
+      const whitelisted =
+        convertValue ||
+        whitelist.keys.some((key) => key === newKey || key === oldKey);
 
-      whitelist.keys.map(key => {
-        if (newKey === key || oldKey === key) {
-          whitelisted = true;
-        }
-      });
-
-      newObject[newKey] = convertCase(
-        oldObject[oldKey],
-        converterFunction,
-        whitelisted
-      );
+      newObject[newKey] = convertCase(oldObject[oldKey], callback, whitelisted);
     });
   }
 
