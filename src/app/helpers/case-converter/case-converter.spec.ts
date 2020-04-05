9fd6aa25b3b6c5c3d00a42d6324587757ff0f436
Charles Alleman <charles@alleman.com.au>
2020-04-05 22:30:46 +1000

    Case Converter Adjustments (#175)
    - Prevented caseConverter function from modifying keys which included '.'
    - Updated test suite to 100% coverage
    - Fixed whitelisted value conversions (was previously not working for arrays and objects)

--- src/app/helpers/case-converter/case-converter.spec.ts
+++ src/app/helpers/case-converter/case-converter.spec.ts
@@ -1,211 +1,170 @@
 import { toCamelCase, toSnakeCase } from "./case-converter";
 
-/**
- * Tests are developed here: https://github.com/travelperk/case-converter
- * @copyright MIT
- */
+const conversionCallbacks = [
+  {
+    name: "to camelCase",
+    callback: toCamelCase,
+    fieldOne: "fieldOne",
+    fieldTwo: "fieldTwo",
+    whitelistKey: "whitelistKey",
+    whitelistValue: "contentOne",
+  },
+  {
+    name: "to snake_case",
+    callback: toSnakeCase,
+    fieldOne: "field_one",
+    fieldTwo: "field_two",
+    whitelistKey: "whitelist_key",
+    whitelistValue: "content_one",
+  },
+];
 
-describe("to camelCase", () => {
-  it("simple objects", () => {
-    const before = { field_one: "content one", "field-two": "content two" };
-    const expected = { fieldOne: "content one", fieldTwo: "content two" };
-    const result = toCamelCase(before);
-    expect(expected).toEqual(result);
-  });
+conversionCallbacks.forEach((test) => {
+  const fieldOne = test.fieldOne;
+  const fieldTwo = test.fieldTwo;
+  const whitelistKey = test.whitelistKey;
+  const whitelistValue = test.whitelistValue;
 
-  it("nested objects", () => {
-    const before = {
-      field_one: {
-        nested_field_one: "content one",
-        nested_field_two: "content two"
-      },
-      field_two: {
-        "nested-field-one": "content one",
-        "nested-field-two": "content two"
-      }
-    };
-    const expected = {
-      fieldOne: {
-        nestedFieldOne: "content one",
-        nestedFieldTwo: "content two"
-      },
-      fieldTwo: { nestedFieldOne: "content one", nestedFieldTwo: "content two" }
-    };
-    const result = toCamelCase(before);
-    expect(expected).toEqual(result);
-  });
+  describe(test.name, () => {
+    it("simple objects", () => {
+      const before = { field_one: "content one", "field-two": "content two" };
+      const expected = { [fieldOne]: "content one", [fieldTwo]: "content two" };
+      const result = test.callback(before);
+      expect(expected).toEqual(result);
+    });
 
-  it("objects in arrays", () => {
-    const before = [
-      { field_one_one: "content one one", field_one_two: "content two two" },
-      { "field-two-one": "content two one", "field-two-two": "content two two" }
-    ];
-    const expected = [
-      { fieldOneOne: "content one one", fieldOneTwo: "content two two" },
-      { fieldTwoOne: "content two one", fieldTwoTwo: "content two two" }
-    ];
-    const result = toCamelCase(before);
-    expect(expected).toEqual(result);
-  });
+    it("nested objects", () => {
+      const before = {
+        field_one: {
+          field_one: "content one",
+          field_two: "content two",
+        },
+        field_two: {
+          "field-one": "content one",
+          "field-two": "content two",
+        },
+      };
+      const expected = {
+        [fieldOne]: {
+          [fieldOne]: "content one",
+          [fieldTwo]: "content two",
+        },
+        [fieldTwo]: {
+          [fieldOne]: "content one",
+          [fieldTwo]: "content two",
+        },
+      };
+      const result = test.callback(before);
+      expect(expected).toEqual(result);
+    });
 
-  it("objects in arrays in arrays", () => {
-    const before = [
-      [{ field_one_one: "content one one", field_one_two: "content two two" }],
-      [
+    it("objects in arrays", () => {
+      const before = [
         {
-          "field-two-one": "content two one",
-          "field-two-two": "content two two"
-        }
-      ]
-    ];
-    const expected = [
-      [{ fieldOneOne: "content one one", fieldOneTwo: "content two two" }],
-      [{ fieldTwoOne: "content two one", fieldTwoTwo: "content two two" }]
-    ];
-    const result = toCamelCase(before);
-    expect(expected).toEqual(result);
-  });
-
-  it("objects in arrays in objects", () => {
-    const before = {
-      an_array: [
-        { field_one_one: "content one one", field_one_two: "content two two" },
+          field_one: "content one one",
+          field_two: "content two two",
+        },
         {
-          "field-two-one": "content two one",
-          "field-two-two": "content two two"
-        }
-      ]
-    };
-    const expected = {
-      anArray: [
-        { fieldOneOne: "content one one", fieldOneTwo: "content two two" },
-        { fieldTwoOne: "content two one", fieldTwoTwo: "content two two" }
-      ]
-    };
-    const result = toCamelCase(before);
-    expect(expected).toEqual(result);
-  });
+          "field-one": "content two one",
+          "field-two": "content two two",
+        },
+      ];
+      const expected = [
+        { [fieldOne]: "content one one", [fieldTwo]: "content two two" },
+        { [fieldOne]: "content two one", [fieldTwo]: "content two two" },
+      ];
+      const result = test.callback(before);
+      expect(expected).toEqual(result);
+    });
 
-  it("undefined and null object", () => {
-    const before = {
-      an_array: [
-        { field_one_one: "content one one", field_one_two: "content two two" },
-        {
-          "field-two-one": "content two one",
-          "field-two-two": "content two two"
-        }
-      ]
-    };
-    const expected = {
-      anArray: [
-        { fieldOneOne: "content one one", fieldOneTwo: "content two two" },
-        { fieldTwoOne: "content two one", fieldTwoTwo: "content two two" }
-      ]
-    };
-    const result = toCamelCase(before);
-    expect(expected).toEqual(result);
-  });
+    it("objects in arrays in arrays", () => {
+      const before = [
+        [
+          {
+            field_one: "content one one",
+            field_two: "content two two",
+          },
+        ],
+        [
+          {
+            "field-one": "content two one",
+            "field-two": "content two two",
+          },
+        ],
+      ];
+      const expected = [
+        [{ [fieldOne]: "content one one", [fieldTwo]: "content two two" }],
+        [{ [fieldOne]: "content two one", [fieldTwo]: "content two two" }],
+      ];
+      const result = test.callback(before);
+      expect(expected).toEqual(result);
+    });
 
-  it("objects containing dates", () => {
-    const date = new Date();
-    const before = { a_date: date };
-    const expected = { aDate: date };
-    const result = toCamelCase(before);
-    expect(result.aDate instanceof Date).toBe(true);
-    expect(expected).toEqual(result);
-  });
-});
+    it("objects in arrays in objects", () => {
+      const before = {
+        field_one: [
+          {
+            field_one: "content one one",
+            field_two: "content two two",
+          },
+          {
+            "field-one": "content two one",
+            "field-two": "content two two",
+          },
+        ],
+      };
+      const expected = {
+        [fieldOne]: [
+          { [fieldOne]: "content one one", [fieldTwo]: "content two two" },
+          { [fieldOne]: "content two one", [fieldTwo]: "content two two" },
+        ],
+      };
+      const result = test.callback(before);
+      expect(expected).toEqual(result);
+    });
 
-describe("to snake_case", () => {
-  it("simple objects", () => {
-    const before = { fieldOne: "content one", "field-two": "content two" };
-    const expected = { field_one: "content one", field_two: "content two" };
-    const result = toSnakeCase(before);
-    expect(expected).toEqual(result);
-  });
+    it("undefined and null object", () => {
+      const before = { field_one: undefined, field_two: null };
+      const expected = { [fieldOne]: undefined, [fieldTwo]: null };
+      const result = test.callback(before);
+      expect(expected).toEqual(result);
+    });
 
-  it("nested objects", () => {
-    const before = {
-      fieldOne: {
-        nestedFieldOne: "content one",
-        nestedFieldTwo: "content two"
-      },
-      fieldTwo: {
-        "nested-field-one": "content one",
-        "nested-field-two": "content two"
-      }
-    };
-    const expected = {
-      field_one: {
-        nested_field_one: "content one",
-        nested_field_two: "content two"
-      },
-      field_two: {
-        nested_field_one: "content one",
-        nested_field_two: "content two"
-      }
-    };
-    const result = toSnakeCase(before);
-    expect(expected).toEqual(result);
-  });
+    it("should not convert .'s", () => {
+      const before = {
+        ["field_one.field_two"]: "content one",
+        ["field-two.field-one"]: "content two",
+      };
+      const expected = {
+        [`${fieldOne}.${fieldTwo}`]: "content one",
+        [`${fieldTwo}.${fieldOne}`]: "content two",
+      };
+      const result = test.callback(before);
+      expect(expected).toEqual(result);
+    });
 
-  it("objects in arrays", () => {
-    const before = [
-      { fieldOneOne: "content one one", fieldOneTwo: "content two two" },
-      { "field-two-one": "content two one", "field-two-two": "content two two" }
-    ];
-    const expected = [
-      { field_one_one: "content one one", field_one_two: "content two two" },
-      { field_two_one: "content two one", field_two_two: "content two two" }
-    ];
-    const result = toSnakeCase(before);
-    expect(expected).toEqual(result);
-  });
+    // TODO Add checking for whitelisted key after conversion
+    describe("whitelist", () => {
+      it("should convert whitelisted value", () => {
+        const before = { ["whitelist-key"]: "content one" };
+        const expected = { [whitelistKey]: whitelistValue };
+        const result = test.callback(before);
+        expect(expected).toEqual(result);
+      });
 
-  it("objects in arrays in arrays", () => {
-    const before = [
-      [{ fieldOneOne: "content one one", fieldOneTwo: "content two two" }],
-      [
-        {
-          "field-two-one": "content two one",
-          "field-two-two": "content two two"
-        }
-      ]
-    ];
-    const expected = [
-      [{ field_one_one: "content one one", field_one_two: "content two two" }],
-      [{ field_two_one: "content two one", field_two_two: "content two two" }]
-    ];
-    const result = toSnakeCase(before);
-    expect(expected).toEqual(result);
-  });
+      it("should convert whitelisted value object", () => {
+        const before = { ["whitelist-key"]: { field_one: "content one" } };
+        const expected = { [whitelistKey]: { [fieldOne]: whitelistValue } };
+        const result = test.callback(before);
+        expect(expected).toEqual(result);
+      });
 
-  it("objects in arrays in objects", () => {
-    const before = {
-      anArray: [
-        { fieldOneOne: "content one one", fieldOneTwo: "content two two" },
-        {
-          "field-two-one": "content two one",
-          "field-two-two": "content two two"
-        }
-      ]
-    };
-    const expected = {
-      an_array: [
-        { field_one_one: "content one one", field_one_two: "content two two" },
-        { field_two_one: "content two one", field_two_two: "content two two" }
-      ]
-    };
-    const result = toSnakeCase(before);
-    expect(expected).toEqual(result);
-  });
-
-  it("objects containing dates", () => {
-    const date = new Date();
-    const before = { aDate: date };
-    const expected = { a_date: date };
-    const result = toSnakeCase(before);
-    expect(result.a_date instanceof Date).toBe(true);
-    expect(expected).toEqual(result);
+      it("should convert whitelisted value array", () => {
+        const before = { ["whitelist-key"]: ["content one"] };
+        const expected = { [whitelistKey]: [whitelistValue] };
+        const result = test.callback(before);
+        expect(expected).toEqual(result);
+      });
+    });
   });
 });
