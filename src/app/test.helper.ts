339718d9afb5ba0d94f471cfedf68c3d9a151d0b
Charles Alleman <charles@alleman.com.au>
2020-04-27 09:53:17 +1000

    Created immutable api mock

--- src/app/test.helper.ts
+++ src/app/test.helper.ts
@@ -1,25 +1,26 @@
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Params } from "@angular/router";
+import { MockImmutableApiService } from "@baw-api/mock/immutableApiMock.service";
 import { BehaviorSubject } from "rxjs";
 import {
   API_CONFIG,
   API_ROOT,
-  CMS_ROOT
+  CMS_ROOT,
 } from "./helpers/app-initializer/app-initializer";
 import { AppConfigService } from "./services/app-config/app-config.service";
 import {
   AppConfigMockService,
-  testApiConfig
+  testApiConfig,
 } from "./services/app-config/appConfigMock.service";
 import { AccountService } from "./services/baw-api/account.service";
 import { BawApiInterceptor } from "./services/baw-api/api.interceptor.service";
 import {
   BawApiService,
-  STUB_MODEL_BUILDER
+  STUB_MODEL_BUILDER,
 } from "./services/baw-api/baw-api.service";
 import {
   MockBawApiService,
-  MockModel
+  MockModel,
 } from "./services/baw-api/mock/baseApiMock.service";
 import { MockSecurityService } from "./services/baw-api/mock/securityMock.service";
 import { MockShowApiService } from "./services/baw-api/mock/showApiMock.service";
@@ -30,7 +31,7 @@ import { ScriptsService } from "./services/baw-api/scripts.service";
 import { SecurityService } from "./services/baw-api/security.service";
 import {
   ShallowSitesService,
-  SitesService
+  SitesService,
 } from "./services/baw-api/sites.service";
 import { TagGroupService } from "./services/baw-api/tag-group.service";
 import { TagsService } from "./services/baw-api/tags.service";
@@ -42,22 +43,22 @@ import { UserService } from "./services/baw-api/user.service";
 export const testAppInitializer = [
   {
     provide: API_ROOT,
-    useValue: testApiConfig.environment.apiRoot
+    useValue: testApiConfig.environment.apiRoot,
   },
   {
     provide: CMS_ROOT,
-    useValue: testApiConfig.environment.cmsRoot
+    useValue: testApiConfig.environment.cmsRoot,
   },
   {
     provide: API_CONFIG,
-    useValue: new Promise(resolve => {
+    useValue: new Promise((resolve) => {
       resolve(testApiConfig);
-    })
+    }),
   },
   {
     provide: AppConfigService,
-    useClass: AppConfigMockService
-  }
+    useClass: AppConfigMockService,
+  },
 ];
 
 /**
@@ -68,19 +69,19 @@ export const testBawServices = [
   {
     provide: HTTP_INTERCEPTORS,
     useClass: BawApiInterceptor,
-    multi: true
+    multi: true,
   },
   { provide: STUB_MODEL_BUILDER, useValue: MockModel },
   { provide: BawApiService, useClass: MockBawApiService },
   { provide: SecurityService, useClass: MockSecurityService },
   { provide: AccountService, useClass: MockStandardApiService },
   { provide: ProjectsService, useClass: MockStandardApiService },
-  { provide: ScriptsService, useClass: MockStandardApiService },
+  { provide: ScriptsService, useClass: MockImmutableApiService },
   { provide: SitesService, useClass: MockStandardApiService },
   { provide: ShallowSitesService, useClass: MockStandardApiService },
   { provide: TagsService, useClass: MockStandardApiService },
   { provide: TagGroupService, useClass: MockStandardApiService },
-  { provide: UserService, useClass: MockShowApiService }
+  { provide: UserService, useClass: MockShowApiService },
 ];
 
 /**
