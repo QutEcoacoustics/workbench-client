a6dfff77bf72c47ab16219e53a9fc0dcc504c47d
Charles Alleman <charles@alleman.com.au>
2020-06-24 11:46:58 +1000

    Add edge browser testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const process = require("process");
-process.env.CHROME_BIN = require("puppeteer").executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: "",
@@ -11,11 +8,10 @@ module.exports = function (config) {
     plugins: [
       require("karma-jasmine"),
       require("karma-viewport"),
-      require("karma-edge-launcher"),
       require("karma-chrome-launcher"),
       require("karma-firefox-launcher"),
-      require("karma-edge-launcher"),
       require("karma-opera-launcher"),
+      require("@chiragrupani/karma-chromium-edge-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage-istanbul-reporter"),
       require("@angular-devkit/build-angular/plugins/karma"),
