b299cd062044817543c339ee0589519937b46c30
Charles Alleman <charles@alleman.com.au>
2020-07-22 14:02:32 +1000

    Working timezone displays

--- src/app/component/shared/formly/timezone-input.component.ts
+++ src/app/component/shared/formly/timezone-input.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FieldType } from "@ngx-formly/core";
+import moment from "moment-timezone";
+import { DateTime, IANAZone } from "luxon";
 
 /**
  * Timezone Input
@@ -7,61 +9,63 @@ import { FieldType } from "@ngx-formly/core";
 @Component({
   // tslint:disable-next-line: component-selector
   selector: "formly-timezone-input",
-  template: ` <p>Timezone Input Placeholder</p> `,
-})
-// template: `
-//   <div class="form-group">
-//     <label *ngIf="field.templateOptions.label" [for]="field.id">
-//       {{
-//         field.templateOptions.label +
-//           (field.templateOptions.required ? " *" : "")
-//       }}
-//     </label>
+  template: `
+    <div class="form-group">
+      <label *ngIf="field.templateOptions.label" [for]="field.id">
+        {{
+          field.templateOptions.label +
+            (field.templateOptions.required ? " *" : "")
+        }}
+      </label>
 
-//     <div class="input-group">
-//       <select
-//         class="w-100 form-control"
-//         [id]="field.id"
-//         [formControl]="formControl"
-//         [formlyAttributes]="field"
-//         (change)="calculateCurrentTime()"
-//       >
-//         <option value=""></option>
-//         <ng-container *ngFor="let timezone of timezones">
-//           <ng-container *ngIf="timezone.zones.length === 1; else group">
-//             <!-- Create singular option -->
-//             <option [value]="timezone.zones[0]">
-//               {{ timezone.iso | iso2CountryPipe }}
-//             </option>
-//           </ng-container>
-//           <ng-template #group>
-//             <!-- Create subgroup of countries with same timezone -->
-//             <optgroup [label]="timezone.iso | iso2CountryPipe">
-//               <option *ngFor="let zone of timezone.zones" [value]="zone">
-//                 {{ timezone.iso | iso2CountryPipe }} -
-//                 {{ formatTimezoneString(zone) }}
-//               </option>
-//             </optgroup>
-//           </ng-template>
-//         </ng-container>
-//       </select>
-//       <div class="input-group-append">
-//         <div class="input-group-text">
-//           <small>{{
-//             model[key] ? offsetOfTimezone(model[key]) : "(no match)"
-//           }}</small>
-//         </div>
-//       </div>
-//     </div>
-//     <small class="form-text text-muted">{{ currentTime }}</small>
-//   </div>
-// `
-// })
+      <div class="input-group">
+        <select
+          class="w-100 form-control"
+          [id]="field.id"
+          [formControl]="formControl"
+          [formlyAttributes]="field"
+          (change)="calculateCurrentTime()"
+        >
+          <option value=""></option>
+          <ng-container *ngFor="let timezone of timezones">
+            <!-- Create singular option -->
+            <option [value]="timezone">
+              {{ timezone }}
+            </option>
+          </ng-container>
+        </select>
+        <div class="input-group-append">
+          <div class="input-group-text">
+            <small>{{ offset }}</small>
+          </div>
+        </div>
+      </div>
+      <small class="form-text text-muted">{{ currentTime }}</small>
+    </div>
+  `,
+})
 // tslint:disable-next-line: component-class-suffix
 export class FormlyTimezoneInput extends FieldType implements OnInit {
+  public defaultTime = "(no match)";
+  public timezones: string[];
+  public currentTime = this.defaultTime;
+  public offset: string = this.defaultTime;
+
   constructor() {
     super();
   }
 
-  public ngOnInit() {}
+  public ngOnInit() {
+    this.timezones = moment.tz.names();
+  }
+
+  public calculateCurrentTime() {
+    const key = this.field.key;
+    const locale = this.field.model[key];
+    const zone = IANAZone.create(locale);
+    const now = DateTime.local().setZone(zone);
+    this.currentTime = now.toFormat("yyyy LLL dd HH:mm");
+    this.offset = zone.formatOffset(now.millisecond, "short");
+    zone.offset(now.millisecond).toString();
+  }
 }
