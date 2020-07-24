fbdd164817f32d45d1d2273c2b75688822b0b6b8
Charles Alleman <charles@alleman.com.au>
2020-07-24 14:40:25 +1000

    Working location input

--- src/app/component/shared/formly/location-input.component.ts
+++ src/app/component/shared/formly/location-input.component.ts
@@ -1,12 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { isInstantiated } from "@helpers/isInstantiated/isInstantiated";
 import { FieldType } from "@ngx-formly/core";
 
 /**
- * Timezone Input
+ * Location Input
  */
 @Component({
   // tslint:disable-next-line: component-selector
-  selector: "formly-timezone-input",
+  selector: "formly-location-input",
   template: `
     <div class="form-group">
       <label for="latitude">
@@ -15,10 +16,20 @@ import { FieldType } from "@ngx-formly/core";
       <input
         id="latitude"
         type="number"
+        class="form-control"
+        [class]="{ 'is-invalid': latitudeError }"
         [formlyAttributes]="field"
         [(ngModel)]="latitude"
-        (ngModelChange)="validateValues()"
+        (ngModelChange)="updateValue()"
       />
+
+      <div
+        *ngIf="latitudeError"
+        class="invalid-feedback"
+        style="display: block;"
+      >
+        {{ getError() }}
+      </div>
 
       <label for="longitude">
         Longitude {{ field.templateOptions.required ? " *" : "" }}
@@ -26,21 +37,76 @@ import { FieldType } from "@ngx-formly/core";
       <input
         id="longitude"
         type="number"
+        class="form-control"
+        [class]="{ 'is-invalid': longitudeError }"
         [formlyAttributes]="field"
         [(ngModel)]="longitude"
-        (ngModelChange)="validateValues()"
+        (ngModelChange)="updateValue()"
       />
+
+      <div
+        *ngIf="longitudeError"
+        class="invalid-feedback"
+        style="display: block;"
+      >
+        {{ getError() }}
+      </div>
 
       <input type="hidden" [id]="field.id" [formControl]="formControl" />
     </div>
   `,
 })
 // tslint:disable-next-line: component-class-suffix
-export class FormlyLocationInput extends FieldType {
+export class FormlyLocationInput extends FieldType implements OnInit {
   public latitude: number;
+  public latitudeError: boolean;
   public longitude: number;
+  public longitudeError: boolean;
 
-  public validateValues() {
-    console.log("Validating values", this.latitude, this.longitude);
+  public ngOnInit() {
+    this.latitude = this.model["latitude"];
+    this.longitude = this.model["longitude"];
+    this.formControl.setValidators(() => {
+      const error = this.validateValues();
+      return error ? { [this.field.key]: error } : null;
+    });
+    this.formControl.updateValueAndValidity();
+  }
+
+  /**
+   * Update hidden input
+   */
+  public updateValue() {
+    this.formControl.setValue({
+      latitude: this.latitude,
+      longitude: this.longitude,
+    });
+    this.model["latitude"] = this.latitude;
+    this.model["longitude"] = this.longitude;
+  }
+
+  public getError(): string {
+    return this.formControl.getError(this.field.key);
+  }
+
+  /**
+   * Validate location values and return error if any
+   */
+  private validateValues(): string {
+    this.latitudeError = false;
+    this.longitudeError = false;
+
+    // XOR if latitude or longitude is set
+    if (!isInstantiated(this.latitude) !== !isInstantiated(this.longitude)) {
+      this.latitudeError = !isInstantiated(this.latitude);
+      this.longitudeError = !isInstantiated(this.longitude);
+      return "Both latitude and longitude must be set or left empty";
+    } else if (this.latitude < -90 || this.latitude > 90) {
+      this.latitudeError = true;
+      return "Latitude must be between -90 and 90";
+    } else if (this.longitude < -180 || this.longitude > 180) {
+      this.longitudeError = true;
+      return "Longitude must be between -180 and 180";
+    }
   }
 }
