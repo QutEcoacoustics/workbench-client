fddeb350c814bf134973c24233649859178d45dc
Charles Alleman <charles@alleman.com.au>
2020-08-19 14:06:32 +1000

    Removed primeng dep

--- src/app/component/projects/projects.module.ts
+++ src/app/component/projects/projects.module.ts
@@ -3,13 +3,10 @@ import { RouterModule } from "@angular/router";
 import { GetRouteConfigForPage } from "@helpers/page/pageRouting";
 import { MapModule } from "@shared/map/map.module";
 import { SharedModule } from "@shared/shared.module";
-import { HarvestCompleteComponent } from "./harvest-complete/harvest-complete.component";
-import { HarvestReviewComponent } from "./harvest-review/harvest-review.component";
 import { AssignComponent } from "./pages/assign/assign.component";
 import { DeleteComponent } from "./pages/delete/delete.component";
 import { DetailsComponent } from "./pages/details/details.component";
 import { EditComponent } from "./pages/edit/edit.component";
-import { HarvestComponent } from "./pages/harvest/harvest.component";
 import { ListComponent } from "./pages/list/list.component";
 import { NewComponent } from "./pages/new/new.component";
 import { PermissionsComponent } from "./pages/permissions/permissions.component";
@@ -23,9 +20,6 @@ const components = [
   DeleteComponent,
   DetailsComponent,
   EditComponent,
-  HarvestCompleteComponent,
-  HarvestComponent,
-  HarvestReviewComponent,
   ListComponent,
   NewComponent,
   PermissionsComponent,
