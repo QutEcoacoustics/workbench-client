658016b50905e7cdcefd93ed79d956ad0e2a9aaf
Charles Alleman <charles@alleman.com.au>
2020-09-01 17:33:21 +1000

    Modified landing page to match suggestions
    Reimplemented home page to reflect design changes provided by @atruskie

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { ProjectsService } from "@baw-api/project/projects.service";
 import { SecurityService } from "@baw-api/security/security.service";
 import { projectsMenuItem } from "@components/projects/projects.menus";
@@ -12,29 +12,54 @@ import { homeCategory, homeMenuItem } from "./home.menus";
 
 @Component({
   selector: "app-home",
+  styles: [
+    `
+      #website-title {
+        color: white;
+        font-weight: bold;
+        font-size: 10rem;
+        font-family: "Arial";
+        filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.7));
+        padding-bottom: 15rem;
+      }
+
+      section {
+        background-color: #fff;
+        filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.7));
+      }
+    `,
+  ],
   template: `
-    <baw-cms [page]="page"></baw-cms>
-    <div class="container-fluid" style="background-color: #e9ecef;">
-      <section class="container">
-        <h2>Some of the projects you can access</h2>
-        <p>
-          You can browse some public projects and audio recordings without
-          logging in. To participate in the analysis work you will need to Log
-          in with an existing account or Register for a new account.
-        </p>
-        <baw-cards [cards]="projectList">
-          <ng-container>
-            <button
-              class="m-auto btn btn-outline-primary"
-              [routerLink]="projectsLink"
-            >
-              More Projects
-            </button>
-          </ng-container>
-        </baw-cards>
-      </section>
+    <div id="hero-image">
+      <baw-cms [page]="page"></baw-cms>
+      <div class="container-fluid" style="padding-bottom: 3rem;">
+        <section
+          class="container"
+          style="background-color: #fff; filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.7));"
+        >
+          <h2>Some of the projects you can access</h2>
+          <p>
+            You can browse some public projects and audio recordings without
+            logging in. To participate in the analysis work you will need to Log
+            in with an existing account or Register for a new account.
+          </p>
+          <baw-cards [cards]="projectList">
+            <ng-container>
+              <button
+                class="m-auto btn btn-outline-primary"
+                [routerLink]="projectsLink"
+              >
+                More Projects
+              </button>
+            </ng-container>
+          </baw-cards>
+        </section>
+      </div>
+      <div></div>
     </div>
   `,
+  // tslint:disable-next-line: use-component-view-encapsulation
+  encapsulation: ViewEncapsulation.None,
 })
 class HomeComponent extends PageComponent implements OnInit {
   public page: string;
