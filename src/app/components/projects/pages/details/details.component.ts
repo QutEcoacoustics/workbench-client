694630cf0a939a9d9ce953c3d64cf00fea119944
Charles Alleman <charles@alleman.com.au>
2020-09-25 10:08:34 +1000

    Extract map marker logic to separate component

--- src/app/components/projects/pages/details/details.component.ts
+++ src/app/components/projects/pages/details/details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import { ApiErrorDetails } from "@baw-api/api.interceptor.service";
 import { projectResolvers } from "@baw-api/project/projects.service";
 import { retrieveResolvers } from "@baw-api/resolver-common";
 import { SitesService } from "@baw-api/site/sites.service";
@@ -19,12 +18,10 @@ import { PaginationTemplate } from "@helpers/paginationTemplate/paginationTempla
 import { Project } from "@models/Project";
 import { Site } from "@models/Site";
 import { NgbPaginationConfig } from "@ng-bootstrap/ng-bootstrap";
-import { MapMarkerOption, sanitizeMapMarkers } from "@shared/map/map.component";
+import { MapMarkerOption } from "@shared/map/map.component";
 import { PermissionsShieldComponent } from "@shared/permissions-shield/permissions-shield.component";
 import { WidgetMenuItem } from "@shared/widget/widgetItem";
 import { List } from "immutable";
-import { merge, Observable } from "rxjs";
-import { switchMap, takeUntil } from "rxjs/operators";
 
 export const projectMenuItemActions = [
   exploreAudioMenuItem,
@@ -61,9 +58,7 @@ class DetailsComponent extends PaginationTemplate<Site> implements OnInit {
       sitesService,
       "name",
       () => [this.project.id],
-      (sites) => {
-        this.sites = List(sites);
-      }
+      (sites) => (this.sites = List(sites))
     );
   }
 
@@ -74,45 +69,6 @@ class DetailsComponent extends PaginationTemplate<Site> implements OnInit {
     }
     this.project = resolvedModels[projectKey] as Project;
     super.ngOnInit();
-
-    this.api
-      .list(this.project)
-      .pipe(
-        switchMap((models) => this.getMarkers(models)),
-        takeUntil(this.unsubscribe)
-      )
-      .subscribe(
-        (sites) => this.pushMarkers(sites),
-        (error: ApiErrorDetails) => {
-          this.error = error;
-          this.loading = false;
-        }
-      );
-  }
-
-  /**
-   * Retrieve map markers from api
-   */
-  private getMarkers(sites: Site[]) {
-    const numPages = sites?.[0]?.getMetadata()?.paging?.maxPage || 1;
-    const observables: Observable<Site[]>[] = [];
-
-    // Can skip first page because initial filter produces the results
-    for (let page = 2; page <= numPages; page++) {
-      observables.push(this.api.filter({ paging: { page } }, this.project));
-    }
-
-    this.pushMarkers(sites);
-    return merge(...observables);
-  }
-
-  /**
-   * Push new sites to markers list
-   */
-  private pushMarkers(sites: Site[]) {
-    this.markers = this.markers.concat(
-      sanitizeMapMarkers(sites.map((site) => site.getMapMarker()))
-    );
   }
 }
 
---
+++ src/app/components/projects/site-map/site-map.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SiteMapComponent } from './site-map.component';
+
+describe('SiteMapComponent', () => {
+  let component: SiteMapComponent;
+  let fixture: ComponentFixture<SiteMapComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SiteMapComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SiteMapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
