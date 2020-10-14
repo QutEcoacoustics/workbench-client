edd4eb72ec3915bfc442a3e818b7c8afa4e46953
Charles Alleman <charles@alleman.com.au>
2020-10-15 09:49:17 +1000

    Cleanup home page
    - Merged new home cms page with home component
    - Cleaned up error handler message to give more help
    - Deleted unused cms pages
    -

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,54 +12,8 @@ import { homeCategory, homeMenuItem } from "./home.menus";
 
 @Component({
   selector: "baw-home",
-  styles: [
-    `
-      #website-title {
-        color: white;
-        font-weight: bold;
-        font-size: 10rem;
-        font-family: "Arial";
-        filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.7));
-        padding-bottom: 15rem;
-      }
-
-      section {
-        background-color: #fff;
-        filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.7));
-      }
-    `,
-  ],
-  template: `
-    <div id="hero-image">
-      <baw-cms [page]="page"></baw-cms>
-      <div class="container-fluid" style="padding-bottom: 3rem;">
-        <section class="container">
-          <h2>Some of the projects you can access</h2>
-          <p>
-            You can browse some public projects and audio recordings without
-            logging in. To participate in the analysis work you will need to Log
-            in with an existing account or Register for a new account.
-          </p>
-          <baw-cards [cards]="projectList">
-            <ng-container>
-              <button
-                class="m-auto btn btn-outline-primary"
-                [routerLink]="projectsLink"
-              >
-                More Projects
-              </button>
-            </ng-container>
-          </baw-cards>
-        </section>
-
-        <div class="float-right">
-          <small class="alert alert-secondary m-0">
-            Photo Credit: QUT Ecoacoustic Staff
-          </small>
-        </div>
-      </div>
-    </div>
-  `,
+  styleUrls: ["./home.component.scss"],
+  templateUrl: "./home.component.html",
   // tslint:disable-next-line: use-component-view-encapsulation
   encapsulation: ViewEncapsulation.None,
 })
