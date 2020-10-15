aaa627d8de7b5d7c26887060c391e7d3bb2a6b1b
Charles Alleman <charles@alleman.com.au>
2020-10-15 12:13:47 +1000

    Update harvest pages using model details

--- src/app/components/sites/pages/harvest/point.component.ts
+++ src/app/components/sites/pages/harvest/point.component.ts
@@ -1,24 +1,72 @@
-import { Component } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { projectResolvers } from "@baw-api/project/projects.service";
+import { regionResolvers } from "@baw-api/region/regions.service";
+import { retrieveResolvers } from "@baw-api/resolver-common";
+import { SecurityService } from "@baw-api/security/security.service";
+import { siteResolvers } from "@baw-api/site/sites.service";
 import {
   pointHarvestMenuItem,
   pointsCategory,
 } from "@components/sites/points.menus";
+import { API_ROOT } from "@helpers/app-initializer/app-initializer";
+import { PageComponent } from "@helpers/page/pageComponent";
+import { PageInfo } from "@helpers/page/pageInfo";
+import { Project } from "@models/Project";
+import { Region } from "@models/Region";
+import { Site } from "@models/Site";
+import { SessionUser } from "@models/User";
 import { List } from "immutable";
 import { pointMenuItemActions } from "../details/point.component";
-import { SiteHarvestComponent } from "./site.component";
+
+const projectKey = "project";
+const regionKey = "region";
+const siteKey = "site";
 
 /**
  * Site Harvest Component
  */
 @Component({
-  selector: "baw-sites-harvest",
+  selector: "baw-points-harvest",
   templateUrl: "./harvest.component.html",
 })
-class PointHarvestComponent extends SiteHarvestComponent {}
+class PointHarvestComponent extends PageComponent implements OnInit {
+  public project: Project;
+  public region: Region;
+  public site: Site;
+  public user: SessionUser;
+
+  constructor(
+    @Inject(API_ROOT) public apiRoot: string,
+    private route: ActivatedRoute,
+    private api: SecurityService
+  ) {
+    super();
+  }
+
+  public ngOnInit() {
+    const resolvers = retrieveResolvers(this.route.snapshot.data as PageInfo);
+    if (resolvers) {
+      this.project = resolvers[projectKey] as Project;
+      this.region = resolvers[regionKey] as Region;
+      this.site = resolvers[siteKey] as Site;
+      this.user = this.api.getLocalUser();
+    }
+  }
+
+  public getHarvestFileRoute() {
+    return "not_implemented";
+  }
+}
 
 PointHarvestComponent.LinkComponentToPageInfo({
   category: pointsCategory,
   menus: { actions: List(pointMenuItemActions) },
+  resolvers: {
+    [projectKey]: projectResolvers.show,
+    [regionKey]: regionResolvers.show,
+    [siteKey]: siteResolvers.show,
+  },
 }).AndMenuRoute(pointHarvestMenuItem);
 
 export { PointHarvestComponent };
--- src/app/components/sites/pages/harvest/site.component.ts
+++ src/app/components/sites/pages/harvest/site.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { projectResolvers } from "@baw-api/project/projects.service";
 import { retrieveResolvers } from "@baw-api/resolver-common";
+import { SecurityService } from "@baw-api/security/security.service";
 import { siteResolvers } from "@baw-api/site/sites.service";
+import { API_ROOT } from "@helpers/app-initializer/app-initializer";
 import { PageComponent } from "@helpers/page/pageComponent";
+import { PageInfo } from "@helpers/page/pageInfo";
+import { Project } from "@models/Project";
+import { Region } from "@models/Region";
 import { Site } from "@models/Site";
+import { SessionUser } from "@models/User";
 import { List } from "immutable";
 import { siteHarvestMenuItem, sitesCategory } from "../../sites.menus";
 import { siteMenuItemActions } from "../details/site.component";
 
+const projectKey = "project";
 const siteKey = "site";
 
 /**
@@ -18,24 +26,40 @@ const siteKey = "site";
   templateUrl: "./harvest.component.html",
 })
 class SiteHarvestComponent extends PageComponent implements OnInit {
+  public project: Project;
+  public region: Region;
   public site: Site;
+  public user: SessionUser;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    @Inject(API_ROOT) public apiRoot: string,
+    private route: ActivatedRoute,
+    private api: SecurityService
+  ) {
     super();
   }
 
   public ngOnInit() {
-    const resolvers = retrieveResolvers(this.route.data);
-    if (resolvers) {
-      this.site = resolvers[siteKey] as Site;
+    const models = retrieveResolvers(this.route.snapshot.data as PageInfo);
+    if (models) {
+      this.project = models[projectKey] as Project;
+      this.site = models[siteKey] as Site;
+      this.user = this.api.getLocalUser();
     }
+  }
+
+  public getHarvestFileRoute() {
+    return "not_implemented";
   }
 }
 
 SiteHarvestComponent.LinkComponentToPageInfo({
   category: sitesCategory,
   menus: { actions: List(siteMenuItemActions) },
-  resolvers: { [siteKey]: siteResolvers.show },
+  resolvers: {
+    [projectKey]: projectResolvers.show,
+    [siteKey]: siteResolvers.show,
+  },
 }).AndMenuRoute(siteHarvestMenuItem);
 
 export { SiteHarvestComponent };
