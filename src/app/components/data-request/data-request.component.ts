21bafc70206fdb49d4e0c9e9acb2fad9300e42b8
Charles Alleman <charles@alleman.com.au>
2020-10-15 12:56:54 +1000

    Remove CMS from data-request

--- src/app/components/data-request/data-request.component.ts
+++ src/app/components/data-request/data-request.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { CMS } from "@baw-api/cms/cms.service";
 import { WithFormCheck } from "@guards/form/form.guard";
 import { PageComponent } from "@helpers/page/pageComponent";
 import { dataRequestCategory, dataRequestMenuItem } from "./data-request.menus";
@@ -9,23 +8,34 @@ import { fields as annotationFields } from "./download-annotations.schema.json";
 @Component({
   selector: "baw-data-request",
   template: `
-    <baw-cms [page]="page"></baw-cms>
+    <h1>Data Request</h1>
+
+    <h2>Annotations Download</h2>
+
+    <p>To download a standard CSV of annotations</p>
+
+    <ol>
+      <li>Navigate to the project you're interested in</li>
+      <li>Then, choose the site you want to download annotations for</li>
+      <li>Finally, click the <i>Download annotations</i> link</li>
+    </ol>
+
     <baw-wip>
       <baw-form
         title="Annotations Download"
         subTitle="Please select the timezone for the CSV file containing annotations for ..."
+        submitLabel="Download Annotations"
         [model]="annotationModel"
         [fields]="annotationFields"
-        [submitLabel]="'Download Annotations'"
         [submitLoading]="annotationLoading"
         (onSubmit)="submitDownloadAnnotation($event)"
       ></baw-form>
       <baw-form
         title="Custom Data Request"
         subTitle="Use this form to request a customized annotations list or other data related to the audio recordings on this website. You <strong>do not need</strong> to use this form if you need the standard <strong>annotations CSV</strong> download. "
+        submitLabel="Submit"
         [model]="requestModel"
         [fields]="requestFields"
-        [submitLabel]="'Submit'"
         [submitLoading]="requestLoading"
         (onSubmit)="submitDataRequest($event)"
       ></baw-form>
@@ -36,7 +46,6 @@ class DataRequestComponent extends WithFormCheck(PageComponent) {
   public annotationLoading: boolean;
   public annotationModel = {};
   public annotationFields = annotationFields;
-  public page = "fix_me";
   public requestLoading: boolean;
   public requestModel = {};
   public requestFields = requestFields;
