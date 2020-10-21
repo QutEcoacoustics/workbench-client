73283e7ab78e9ef13ecb297ebc33dde464eae342
Charles Alleman <charles@alleman.com.au>
2020-10-21 10:38:08 +1000

    Update src/app/components/shared/cms/cms.component.ts
    Co-authored-by: Anthony Truskinger <anthony.truskinger@gmail.com>

--- src/app/components/shared/cms/cms.component.ts
+++ src/app/components/shared/cms/cms.component.ts
@@ -39,9 +39,13 @@ export class CmsComponent extends WithUnsubscribe() implements OnInit {
 
     this.cms.get(this.page).subscribe(
       (blob) => {
-        // Using html fragments instead of angular sanitization sanitization
-        // uses innerHTML to insert html blobs, and this will not run <script>
-        // tags. This is a safety feature of innerHTML and cannot be bypassed.
+        // Using html fragments instead of innerHTML. 
+        // In the HTML5 spec, script tags that are inserted via InnerHTML will not be executed.
+        // Using a document fragment allows us to insert any tag.
+        // NOTE: Angular's Sanitization is ignored since we are bypassing Angular's normal rendering system.
+        // NOTE: It might be useful to consider using ShadowDom to isolate these CMS HTML fragments from 
+        //       the rest of the site. This would prevent, for example, a careless CSS global style in the CMS fragment
+        //       from affecting the rest of the angular site.
         const range = document.createRange();
         const fragment = range.createContextualFragment(blob);
         this.renderer.appendChild(this.elRef.nativeElement, fragment);
