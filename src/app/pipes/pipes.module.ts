4aa9431269b2f31615177eec86f57f685b19e796
Charles Alleman <charles@alleman.com.au>
2020-11-02 18:07:03 +1000

    Remove spinner pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from "@angular/core";
 import { IsUnresolvedPipe } from "./isUnresolved/is-unresolved.pipe";
-import { SpinnerPipe } from "./spinner/spinner.pipe";
 import { ToRelativePipe } from "./toRelative/to-relative.pipe";
 
-const pipes = [IsUnresolvedPipe, ToRelativePipe, SpinnerPipe];
+const pipes = [IsUnresolvedPipe, ToRelativePipe];
 
 @NgModule({ declarations: pipes, exports: pipes })
 export class PipesModule {}
