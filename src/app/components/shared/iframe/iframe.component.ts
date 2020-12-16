40fe998906fad744c8e89d094331fd9bd2bca54a
Charles Alleman <charles@alleman.com.au>
2020-12-16 16:39:48 +1000

    Implemented iframe resizing
    Introduces a visual bug when the view size causes the menus to collapse. The action menu receives a weird amount of space between itself and the secondary menu which is closed up as the page section takes up more room.

--- src/app/components/shared/iframe/iframe.component.ts
+++ src/app/components/shared/iframe/iframe.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { Router } from "@angular/router";
 import { withUnsubscribe } from "@helpers/unsubscribe/unsubscribe";
 import { assetRoot } from "@services/app-config/app-config.service";
+import { interval } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: "baw-iframe",
-  template: '<div><iframe [attr.src]="url"></iframe></div>',
+  template: `
+    <iframe #content [src]="url" (load)="updateIframeSize()"></iframe>
+  `,
   styles: [
     `
-      div {
-        height: 100%;
-        display: flex;
-      }
       iframe {
         width: 100%;
-        height: 100%;
+        min-height: 100%;
         border: 0px;
       }
     `,
   ],
 })
 export class IFrameComponent extends withUnsubscribe() implements OnInit {
+  @ViewChild("content") private iframeRef: ElementRef<HTMLIFrameElement>;
   public url: SafeResourceUrl;
+  /** Used to track the height of the iframe */
+  private previousHeight = 0;
 
   public constructor(private router: Router, private sanitizer: DomSanitizer) {
     super();
@@ -33,5 +36,26 @@ export class IFrameComponent extends withUnsubscribe() implements OnInit {
     const requestedUrl = assetRoot + "/old-client/#" + this.router.url;
     // Bypass angular default security
     this.url = this.sanitizer.bypassSecurityTrustResourceUrl(requestedUrl);
+
+    // Update iframe size every 250 milliseconds
+    interval(250)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => {
+        this.updateIframeSize();
+      });
+  }
+
+  public updateIframeSize() {
+    const iframe = this.iframeRef?.nativeElement;
+    const currentHeight = iframe?.contentDocument?.body?.scrollHeight;
+
+    // If iframe has not been built, current height is not available, or no change in size
+    if (!iframe || !currentHeight || currentHeight === this.previousHeight) {
+      return;
+    }
+
+    const padding = 50;
+    this.previousHeight = currentHeight;
+    iframe.style.height = currentHeight + padding + "px";
   }
 }
