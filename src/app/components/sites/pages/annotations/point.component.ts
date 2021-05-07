b5a0e955e04cc101432651de6723bc46012d0a72
Charles Alleman <charles@alleman.com.au>
2021-05-07 17:01:52 +1000

    Cleanup edge case scenario bug

--- src/app/components/sites/pages/annotations/point.component.ts
+++ src/app/components/sites/pages/annotations/point.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { projectResolvers } from "@baw-api/project/projects.service";
+import { regionResolvers } from "@baw-api/region/regions.service";
 import { siteResolvers } from "@baw-api/site/sites.service";
 import {
   pointAnnotationsMenuItem,
@@ -8,19 +9,39 @@ import {
 } from "@components/sites/points.menus";
 import { PermissionsShieldComponent } from "@menu/permissions-shield.component";
 import { WidgetMenuItem } from "@menu/widgetItem";
+import { Region } from "@models/Region";
 import { List } from "immutable";
 import { pointMenuItemActions } from "../details/point.component";
 import { SiteAnnotationsComponent } from "./site.component";
 
 const projectKey = "project";
+const regionKey = "region";
 const siteKey = "site";
 
 @Component({
   selector: "baw-point-annotations",
   templateUrl: "annotations.component.html",
 })
-class PointAnnotationsComponent extends SiteAnnotationsComponent {
-  public isSite = false;
+class PointAnnotationsComponent
+  extends SiteAnnotationsComponent
+  implements OnInit {
+  public ngOnInit(): void {
+    super.ngOnInit();
+
+    if (this.failure) {
+      return;
+    }
+
+    this.region = this.models[regionKey] as Region;
+  }
+
+  public getAnnotationsPath(): string {
+    return this.siteApi.downloadAnnotations(
+      this.site,
+      this.region.projectId,
+      this.model.timezone
+    );
+  }
 }
 
 PointAnnotationsComponent.linkComponentToPageInfo({
@@ -31,6 +52,7 @@ PointAnnotationsComponent.linkComponentToPageInfo({
   },
   resolvers: {
     [projectKey]: projectResolvers.show,
+    [regionKey]: regionResolvers.show,
     [siteKey]: siteResolvers.show,
   },
 }).andMenuRoute(pointAnnotationsMenuItem);
--- src/app/components/sites/pages/annotations/site.component.ts
+++ src/app/components/sites/pages/annotations/site.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { projectResolvers } from "@baw-api/project/projects.service";
-import { retrieveResolvers } from "@baw-api/resolver-common";
+import { ResolvedModelList, retrieveResolvers } from "@baw-api/resolver-common";
 import { siteResolvers, SitesService } from "@baw-api/site/sites.service";
 import {
   siteAnnotationsMenuItem,
@@ -14,6 +14,7 @@ import { PageInfo } from "@helpers/page/pageInfo";
 import { PermissionsShieldComponent } from "@menu/permissions-shield.component";
 import { WidgetMenuItem } from "@menu/widgetItem";
 import { Project } from "@models/Project";
+import { Region } from "@models/Region";
 import { Site } from "@models/Site";
 import { FormlyFieldConfig } from "@ngx-formly/core";
 import { List } from "immutable";
@@ -32,16 +33,17 @@ const siteKey = "site";
   templateUrl: "annotations.component.html",
 })
 class SiteAnnotationsComponent extends PageComponent implements OnInit {
-  public model: TimezoneModel = { timezone: "UTC" };
+  public failure: boolean;
   public fields: FormlyFieldConfig[] = fields;
   public form = new FormGroup({});
-  public failure: boolean;
-  public isSite = true;
+  public model: TimezoneModel = { timezone: "UTC" };
+  public region: Region;
+  protected models: ResolvedModelList;
   protected project: Project;
   protected site: Site;
 
   public constructor(
-    private siteApi: SitesService,
+    protected siteApi: SitesService,
     private route: ActivatedRoute
   ) {
     super();
@@ -55,6 +57,7 @@ class SiteAnnotationsComponent extends PageComponent implements OnInit {
       this.failure = true;
       return;
     }
+    this.models = models;
     this.project = models[projectKey] as Project;
     this.site = models[siteKey] as Site;
     this.model.timezone = this.site.tzinfoTz ?? this.model.timezone;
