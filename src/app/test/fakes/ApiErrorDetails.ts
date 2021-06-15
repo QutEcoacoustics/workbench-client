d3b8018d10ee8fcc4dd69d12e648733207c05e6d
Charles Alleman <charles@alleman.com.au>
2021-06-15 16:34:58 +1000

    Update error handler tests

--- src/app/test/fakes/ApiErrorDetails.ts
+++ src/app/test/fakes/ApiErrorDetails.ts
@@ -1,17 +1,53 @@
 import { ApiErrorDetails } from "@baw-api/api.interceptor.service";
 import { ApiResponse, unknownErrorCode } from "@baw-api/baw-api.service";
-import httpCodes from "http-status";
+import httpCodes, {
+  BAD_REQUEST,
+  FORBIDDEN,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  REQUEST_TIMEOUT,
+  UNAUTHORIZED,
+  UNPROCESSABLE_ENTITY,
+} from "http-status";
 
-// TODO Replace this with http-status numbers instead of string typing
+// TODO Make this is the default method for error generation
+export function generateApiErrorDetailsV2(
+  status: number,
+  custom?: Partial<ApiErrorDetails>
+): ApiErrorDetails {
+  let message: string;
+  switch (status) {
+    case UNPROCESSABLE_ENTITY:
+      message = "Record could not be saved";
+      break;
+    case INTERNAL_SERVER_ERROR:
+      message = "Internal Server Failure";
+      break;
+    case unknownErrorCode:
+      message = "Unknown";
+      break;
+  }
+
+  return {
+    status: custom?.status ?? status,
+    message: custom?.message ?? message ?? (httpCodes[status] as string),
+    info: custom?.info ?? undefined,
+  };
+}
+
+// TODO Remove
 type HTTPStatus =
   | "Unauthorized"
   | "Not Found"
   | "Bad Request"
+  | "Request Timeout"
   | "Unprocessable Entity"
   | "Internal Server Error"
+  | "Forbidden"
   | "Custom";
 
 /**
+ * TODO Remove this function
  * Used to generate a well constructed error object after the interceptor
  * has intercepted and converted the API response
  *
@@ -27,23 +63,31 @@ export function generateApiErrorDetails(
 
   switch (type) {
     case "Unauthorized":
-      status = httpCodes.UNAUTHORIZED;
+      status = UNAUTHORIZED;
       message = type;
       break;
     case "Not Found":
-      status = httpCodes.NOT_FOUND;
+      status = NOT_FOUND;
       message = type;
       break;
     case "Bad Request":
-      status = httpCodes.BAD_REQUEST;
+      status = BAD_REQUEST;
+      message = type;
+      break;
+    case "Forbidden":
+      status = FORBIDDEN;
+      message = type;
+      break;
+    case "Request Timeout":
+      status = REQUEST_TIMEOUT;
       message = type;
       break;
     case "Unprocessable Entity":
-      status = httpCodes.UNPROCESSABLE_ENTITY;
+      status = UNPROCESSABLE_ENTITY;
       message = "Record could not be saved";
       break;
     case "Internal Server Error":
-      status = httpCodes.INTERNAL_SERVER_ERROR;
+      status = INTERNAL_SERVER_ERROR;
       message = "Internal Server Failure";
       break;
   }
