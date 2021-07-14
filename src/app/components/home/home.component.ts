ce7b03d8d74cc036e20a00ce9cff2312a680f59e
Charles Alleman <charles@businessinmind.com.au>
2021-07-14 17:33:00 +1000

    Updated home page title SVG

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { ProjectsService } from "@baw-api/project/projects.service";
 import { SecurityService } from "@baw-api/security/security.service";
 import { projectsMenuItem } from "@components/projects/projects.menus";
 import { PageComponent } from "@helpers/page/pageComponent";
+import { ConfigService } from "@services/config/config.service";
 import { Card } from "@shared/cards/cards.component";
 import { List } from "immutable";
 import { map, mergeMap, takeUntil } from "rxjs/operators";
@@ -16,17 +17,21 @@ import { homeCategory, homeMenuItem } from "./home.menus";
 })
 class HomeComponent extends PageComponent implements OnInit {
   public page = CMS.home;
+  public title: string;
   public projectsLink = projectsMenuItem.route;
   public projectList: List<Card> = List([]);
 
   public constructor(
     private projectApi: ProjectsService,
-    private securityApi: SecurityService
+    private securityApi: SecurityService,
+    public config: ConfigService
   ) {
     super();
   }
 
   public ngOnInit() {
+    this.title = this.config.settings.brand.short.toLocaleUpperCase();
+
     /**
      * TODO Sort projects using the following format:
      * sorted by:
@@ -50,6 +55,11 @@ class HomeComponent extends PageComponent implements OnInit {
         (cards) => (this.projectList = cards),
         () => (this.projectList = List([]))
       );
+  }
+
+  public get viewBox(): string {
+    const width = Math.max(70, Math.floor(this.title.length * (157 / 15)));
+    return "0 0 " + width + " 17";
   }
 }
 
