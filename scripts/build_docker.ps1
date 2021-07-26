028e6577558f212a6bb5fc20bcf42bf91121df06
test <charles@businessinmind.com.au>
2021-07-26 13:55:08 +1000

    Cleanup string formatting

--- scripts/build_docker.ps1
+++ scripts/build_docker.ps1
@@ -21,28 +21,37 @@ function Test-CI() {
 
 . $PSScriptRoot/exec.ps1
 $version = . $PSScriptRoot/version.ps1 $release_tag
-
 Write-Output $version
 
 # tag version on github
+Write-Output "Create git tag"
 if (Test-CI) {
   git config --global user.name "$github_actor"
   git config --global user.email "$github_actor@users.noreply.github.com"
 }
-exec { git tag -a $version.Version -m "Build (${version.DescribeVersion})" }
+$git_tag_name = $version.Version
+$git_tag_message = [string]::Format("Build {0}", $version.DescribeVersion);
+exec { git tag -a "$git_tag_name" -m "$git_tag_message" }
 
 # docker containing and push to docker hub
+Write-Output "Build docker container"
+$docker_name = "qutecoacoustics/workbench-client";
+$docker_release_tag = [string]::Format("{0}:{1}", $docker_name, $release_tag);
+$docker_version_tag = [string]::Format("{0}:{1}", $docker_name, $version.DockerTag);
+
 exec {
   docker build `
-    -t "qutecoacoustics/workbench-client:$release_tag" `
-    -t "qutecoacoustics/workbench-client:$($version.DockerTag)" `
+    -t "$docker_release_tag" `
+    -t "$docker_version_tag" `
     . `
-    --build-arg GIT_COMMIT=$version.LongHash `
-    --build-arg WORKBENCH_CLIENT_VERSION=$version.DescribeVersion
+    --build-arg GIT_COMMIT="$($version.LongHash)" `
+    --build-arg WORKBENCH_CLIENT_VERSION="$($version.DescribeVersion)"
 }
 
-docker push "qutecoacoustics/workbench-client:$release_tag"
-docker push "qutecoacoustics/workbench-client:$($version.DockerTag)"
+Write-Output "Push docker image"
+exec { docker push "$docker_release_tag" }
+exec { docker push "$docker_version_tag" }
 
 # push tag to github
-git push origin --follow-tags "$($version.Version)"
+Write-Output "Push git tag"
+git push origin --follow-tags "$git_tag_name"
