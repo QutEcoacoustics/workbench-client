935a6e91a8e488d7426a2c2fdc9f635546b13d55
Charles Alleman <charles@businessinmind.com.au>
2021-09-01 23:39:58 +1000

    Update defaultMenu

--- src/app/helpers/page/defaultMenus.ts
+++ src/app/helpers/page/defaultMenus.ts
@@ -6,8 +6,6 @@ import {
   myAccountMenuItem,
   myAnnotationsMenuItem,
 } from "@components/profile/profile.menus";
-import { projectsMenuItem } from "@components/projects/projects.menus";
-import { shallowRegionsMenuItem } from "@components/regions/regions.menus";
 import { reportProblemMenuItem } from "@components/report-problem/report-problem.menus";
 import {
   loginMenuItem,
@@ -16,20 +14,18 @@ import {
 import { sendAudioMenuItem } from "@components/send-audio/send-audio.menus";
 import { statisticsMenuItem } from "@components/statistics/statistics.menus";
 import { NavigableMenuItem } from "@interfaces/menusInterfaces";
-import { List } from "immutable";
+import { Set } from "immutable";
 
 /**
  * Default secondary menu items
  */
 export const defaultMenu = {
-  contextLinks: List<NavigableMenuItem>([
+  contextLinks: Set<NavigableMenuItem>([
     homeMenuItem,
     loginMenuItem,
     registerMenuItem,
     myAccountMenuItem,
     myAnnotationsMenuItem,
-    projectsMenuItem,
-    shallowRegionsMenuItem,
     audioAnalysesMenuItem,
     libraryMenuItem,
     dataRequestMenuItem,
