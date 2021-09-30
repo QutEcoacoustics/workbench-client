eb86e726defa879792eb9620f979616a7e24cdde
Charles Alleman <charles@businessinmind.com.au>
2021-09-30 12:01:20 +1000

    Fix broken page not found route

--- src/app/components/error/page-not-found.component.ts
+++ src/app/components/error/page-not-found.component.ts
@@ -1,13 +1,23 @@
 import { Component } from "@angular/core";
 import { homeCategory } from "@components/home/home.menus";
+import { reportProblemMenuItem } from "@components/report-problem/report-problem.menus";
 import { PageComponent } from "@helpers/page/pageComponent";
 import { pageNotFoundMenuItem } from "./error.menus";
 
 @Component({
   selector: "baw-page-not-found",
-  template: "<div>Not Found! TODO: make me better!</div>",
+  template: `
+    <h1>Not Found</h1>
+    <div>
+      This page doesn't seem to exist, if you believe this is an error please go
+      to the <a [strongRoute]="reportProblem">Report Problems</a> page and
+      report the issue.
+    </div>
+  `,
 })
-class PageNotFoundComponent extends PageComponent {}
+class PageNotFoundComponent extends PageComponent {
+  public reportProblem = reportProblemMenuItem.route;
+}
 
 PageNotFoundComponent.linkComponentToPageInfo({
   category: homeCategory,
