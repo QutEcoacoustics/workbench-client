3760a5ef52f825252db702ef5140944540a5db80
Charles Alleman <charles@businessinmind.com.au>
2022-02-25 15:40:04 +1000

    Fixed tests

--- src/app/services/baw-api/site/sites.service.spec.ts
+++ src/app/services/baw-api/site/sites.service.spec.ts
@@ -1,26 +1,19 @@
 import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { IdOr, setAuthorizationQSP, setTimezoneQSP } from "@baw-api/api-common";
+import { IdOr, setTimezoneQSP } from "@baw-api/api-common";
 import { BawApiService } from "@baw-api/baw-api.service";
 import { BawSessionService } from "@baw-api/baw-session.service";
 import { API_ROOT } from "@helpers/app-initializer/app-initializer";
 import { Project } from "@models/Project";
 import { Region } from "@models/Region";
 import { Site } from "@models/Site";
-import {
-  createServiceFactory,
-  mockProvider,
-  SpectatorService,
-} from "@ngneat/spectator";
+import { SpectatorService, createServiceFactory, mockProvider } from "@ngneat/spectator";
 import { MockAppConfigModule } from "@services/config/configMock.module";
 import { generateProject } from "@test/fakes/Project";
 import { generateSite } from "@test/fakes/Site";
-import {
-  validateCustomApiFilter,
-  validateStandardApi,
-} from "@test/helpers/api-common";
-import { modelData } from "@test/helpers/faker";
+import { validateStandardApi, validateCustomApiFilter } from "@test/helpers/api-common";
 import { ToastrService } from "ngx-toastr";
 import { SitesService } from "./sites.service";
+
 
 type Model = Site;
 type Params = [IdOr<Project>];
@@ -72,26 +65,19 @@ describe("SitesService", (): void => {
   describe("downloadAnnotations", () => {
     const defaultTimezone = "UTC";
 
-    function setLoggedIn(authToken: string) {
-      spyOnProperty(session, "authToken").and.returnValue(authToken);
-    }
-
-    function getUrl(timezone: string = defaultTimezone, token?: string) {
+    function getUrl(timezone: string = defaultTimezone) {
       const url = new URL(`${apiRoot}${showUrl}/audio_events/download`);
       setTimezoneQSP(url, timezone);
-      setAuthorizationQSP(url, token);
       return url.toString();
     }
 
     it("should return url using model ids", () => {
-      setLoggedIn(null);
       expect(service.downloadAnnotations(10, 5, defaultTimezone)).toBe(
         getUrl()
       );
     });
 
     it("should return url using model objects", () => {
-      setLoggedIn(null);
       expect(
         service.downloadAnnotations(
           new Site(generateSite({ id: 10 })),
@@ -101,16 +87,7 @@ describe("SitesService", (): void => {
       ).toBe(getUrl());
     });
 
-    it("should return url with auth token appended", () => {
-      const authToken = modelData.authToken();
-      setLoggedIn(authToken);
-      expect(service.downloadAnnotations(10, 5, defaultTimezone)).toBe(
-        getUrl(defaultTimezone, authToken)
-      );
-    });
-
     it("should return url with timezone", () => {
-      setLoggedIn(null);
       expect(service.downloadAnnotations(10, 5, "Brisbane")).toBe(
         getUrl("Brisbane")
       );
