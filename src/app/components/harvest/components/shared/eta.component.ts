45d73878f4e88c99f1c09ffeee5cd1e24e1ae581
Charles Alleman <charles@businessinmind.com.au>
2022-07-15 11:05:41 +1000

    Updated eta component to update every 1 second

--- src/app/components/harvest/components/shared/eta.component.ts
+++ src/app/components/harvest/components/shared/eta.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input } from "@angular/core";
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
+import { isInstantiated } from "@helpers/isInstantiated/isInstantiated";
 import { toRelative } from "@interfaces/apiInterfaces";
 import { Harvest, HarvestStatus } from "@models/Harvest";
 import { Duration } from "luxon";
+import { map, Observable, timer } from "rxjs";
 
 @Component({
   selector: "baw-harvest-eta",
   template: `
     <div class="clearfix">
       <small class="text-muted font-monospace">
-        {{ timeTaken }} {{ humanizeStage }}
+        {{ timeTaken$ | async }} {{ humanizeStage }}
       </small>
       <small *ngIf="!hasProgress()" class="float-end text-muted font-monospace">
         waiting in queue
@@ -19,9 +27,12 @@ import { Duration } from "luxon";
     </div>
   `,
 })
-export class EtaComponent {
+export class EtaComponent implements OnInit, OnChanges {
   @Input() public harvest: Harvest;
   @Input() public progress: number;
+
+  public timeTaken$: Observable<string>;
+  public expectedRemainingTime$: Observable<string>;
 
   public get humanizeStage(): string {
     const humanizedText: Partial<Record<HarvestStatus, string>> = {
@@ -32,28 +43,45 @@ export class EtaComponent {
     return humanizedText[this.harvest.status];
   }
 
-  public get timeTaken(): string {
-    // TODO Use https://github.com/QutEcoacoustics/baw-server/issues/604
-    // for calculation
-    return toRelative(this.harvest.updatedAt.diffNow(), {
-      round: true,
-      largest: 2,
-    });
+  public expectedRemainingTime: string;
+
+  public ngOnInit(): void {
+    this.timeTaken$ = timer(0, 1000).pipe(
+      map((): string =>
+        // TODO Use https://github.com/QutEcoacoustics/baw-server/issues/604
+        // for calculation
+        toRelative(this.harvest.updatedAt.diffNow(), {
+          round: true,
+          largest: 2,
+        })
+      )
+    );
   }
 
-  public get expectedRemainingTime(): string {
-    const progress = this.progress / 100;
-
-    if (progress === 0) {
-      return "unknown time";
+  public ngOnChanges({ progress }: SimpleChanges): void {
+    // Only update on changes to the progress
+    if (!isInstantiated(progress)) {
+      return;
     }
 
-    // TODO Use https://github.com/QutEcoacoustics/baw-server/issues/604
-    // for calculation
+    if (this.progress === 0) {
+      this.expectedRemainingTime = "unknown time";
+    }
+
+    /*
+     * TODO Use https://github.com/QutEcoacoustics/baw-server/issues/604
+     * for calculation
+     * TODO We should average over recent window of harvest items times time
+     * taken to transition (ie, using the latest 10-15 reports to calculate
+     * items/second). The current solution can be extremely inaccurate when
+     * there is a delay or slowdown of processing items (ie. another
+     * harvest is queued and takes up all of the available computing
+     * resources)
+     */
     const timeTakenSeconds = this.harvest.updatedAt.diffNow("seconds").seconds;
     const expectedRemainingTimeSeconds =
-      timeTakenSeconds * (1 / progress) - timeTakenSeconds;
-    return toRelative(
+      timeTakenSeconds * (1 / (this.progress / 100)) - timeTakenSeconds;
+    this.expectedRemainingTime = toRelative(
       Duration.fromMillis(expectedRemainingTimeSeconds * 1000),
       { round: true, largest: 2 }
     );
