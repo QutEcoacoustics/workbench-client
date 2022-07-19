04becf25bb46dec918452d5115bf244a2b273b70
Charles Alleman <charles@businessinmind.com.au>
2022-07-19 04:41:08 +0100

    Updated deps
    Beta version of bootstrap
    
    Revert change to test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: "enabled",
+      initialNavigation: "enabledBlocking",
       scrollPositionRestoration: "enabled",
     }),
   ],
