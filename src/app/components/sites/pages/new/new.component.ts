c05b6e31ab0ee4ee6e37c9d259f5632fe7eb74e8
hudson-newey <grath@duck.com>
2024-09-06 12:28:17 +1000

    Fix new site form not submitting with project_ids
    Following a change to the baw-server which required an array of
    project_ids to be sent while site creation, the new site form started
    throwing errors which stopped users from creating new sites.
    
    This commit now sends project_ids when creating a new site, fixing the
    bug which stopped users from creating new sites.
    
    Fixes: #2143

--- src/app/components/sites/pages/new/new.component.ts
+++ src/app/components/sites/pages/new/new.component.ts
@@ -40,7 +40,23 @@ class SiteNewComponent extends FormTemplate<Site> implements OnInit {
       successMsg: (model) => defaultSuccessMsg("created", model.name),
       redirectUser: (model) =>
         this.router.navigateByUrl(model.getViewUrl(this.project)),
-      getModel: () => (this.region ? { regionId: this.region.id } : {}),
+      getModel: () => {
+        // TODO: for some reason if we use a set here, the project ids don't get
+        // serialized correctly when sending the model body
+        // to get around this, I've used an array and cast to any
+        // we should figure out why we need this type casing and fix it in the
+        // correct way
+        const projectIds = [this.project.id] as any;
+
+        if (this.region) {
+          return {
+            regionId: this.region.id,
+            projectIds,
+          };
+        }
+
+        return { projectIds };
+      },
     });
   }
 
