f39f03cad66386924a4d381804478f2faaaf6a0c
hudson-newey <grath@duck.com>
2025-02-12 10:35:01 +1000

    Add annotation smoke tests

--- src/app/components/import-annotations/add-annotations/add-annotations.component.spec.ts
+++ src/app/components/import-annotations/add-annotations/add-annotations.component.spec.ts
@@ -1,15 +1,19 @@
-import { createComponentFactory, Spectator } from "@ngneat/spectator";
+import { createRoutingFactory, Spectator } from "@ngneat/spectator";
 import { SharedModule } from "@shared/shared.module";
 import { MockBawApiModule } from "@baw-api/baw-apiMock.module";
 import { InlineListComponent } from "@shared/inline-list/inline-list.component";
 import { TypeaheadInputComponent } from "@shared/typeahead-input/typeahead-input.component";
 import { LoadingComponent } from "@shared/loading/loading.component";
+import { ToastrService } from "ngx-toastr";
+import { AudioEventImport } from "@models/AudioEventImport";
 import { AddAnnotationsComponent } from "./add-annotations.component";
 
 describe("AddAnnotationsComponent", () => {
   let spectator: Spectator<AddAnnotationsComponent>;
 
-  const createComponent = createComponentFactory({
+  let audioEventImport: AudioEventImport;
+
+  const createComponent = createRoutingFactory({
     component: AddAnnotationsComponent,
     declarations: [
       InlineListComponent,
@@ -17,6 +21,10 @@ describe("AddAnnotationsComponent", () => {
       LoadingComponent,
     ],
     imports: [SharedModule, MockBawApiModule],
+    mocks: [ToastrService],
+    data: {
+      resolvers: { model: audioEventImport },
+    }
   });
 
   function setup(): void {
