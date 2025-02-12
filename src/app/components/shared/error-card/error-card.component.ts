e2e96622a11c8bcea6a1d875e4240b9596a13626
hudson-newey <grath@duck.com>
2025-02-12 15:40:34 +1000

    Fix error card tests

--- src/app/components/shared/error-card/error-card.component.ts
+++ src/app/components/shared/error-card/error-card.component.ts
@@ -4,22 +4,14 @@ import { BawDataError } from "@interfaces/apiInterfaces";
 @Component({
   selector: "baw-error-card",
   template: `
-    @if (errors().length === 0) {
+    @if (showSuccessState() && errors().length === 0) {
       <div class="error-card callout callout-success">
         No errors
       </div>
     }
 
-    <!-- prettier-ignore -->
     @for (error of errors(); track error) {
       @for (errorKey of extractErrorKeys(error); track errorKey) {
-        <!-- <summary>
-          <strong>{{ errorKey }}</strong>
-          <span class="chevron">
-            <fa-icon [icon]="['fas', 'chevron-up']"></fa-icon>
-          </span>
-        </summary> -->
-
         <div class="error-card callout callout-danger">
           {{ errorKey }}:
           {{ extractErrorValues(error, errorKey) }}
@@ -32,6 +24,7 @@ import { BawDataError } from "@interfaces/apiInterfaces";
 })
 export class ErrorCardComponent {
   public errors = input.required<BawDataError[]>();
+  public showSuccessState = input<boolean>(false);
 
   protected extractErrorKeys(error: BawDataError) {
     return Object.keys(error);
