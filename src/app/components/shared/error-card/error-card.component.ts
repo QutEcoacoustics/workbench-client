513ba5bcfb9eff929cfda66eabdc528054bc3863
hudson-newey <grath@duck.com>
2025-02-12 16:37:52 +1000

    Fix loading state when no files were uploaded

--- src/app/components/shared/error-card/error-card.component.ts
+++ src/app/components/shared/error-card/error-card.component.ts
@@ -1,18 +1,25 @@
 import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 import { BawDataError } from "@interfaces/apiInterfaces";
 
+export enum ErrorCardStyle {
+  /** Styles the error card to fit inside a table cell */
+  Inline,
+  /** Styles the error card as a standalone alert notification */
+  Alert,
+}
+
 @Component({
   selector: "baw-error-card",
   template: `
     @if (showSuccessState() && errors().length === 0) {
-      <div class="error-card callout callout-success">
+      <div class="error-card {{ divStyle }} {{ divStyle }}-success">
         No errors
       </div>
     }
 
     @for (error of errors(); track error) {
       @for (errorKey of extractErrorKeys(error); track errorKey) {
-        <div class="error-card callout callout-danger">
+        <div class="error-card {{ divStyle }} {{ divStyle }}-danger">
           {{ errorKey }}:
           {{ extractErrorValues(error, errorKey) }}
         </div>
@@ -24,7 +31,14 @@ import { BawDataError } from "@interfaces/apiInterfaces";
 })
 export class ErrorCardComponent {
   public errors = input.required<BawDataError[]>();
+  public errorStyle = input<ErrorCardStyle>(ErrorCardStyle.Alert);
   public showSuccessState = input<boolean>(false);
+
+  protected get divStyle(): string {
+    return this.errorStyle() === ErrorCardStyle.Alert
+      ? "alert"
+      : "callout";
+  }
 
   protected extractErrorKeys(error: BawDataError) {
     return Object.keys(error);
