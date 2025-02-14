369ba4a36e648e1d5e9c21af4be3638fecde472f
hudson-newey <grath@duck.com>
2025-02-14 10:20:14 +1000

    Fix virtual datatable pagination file casing

--- src/app/directives/datatable/datatable.module.ts
+++ src/app/directives/datatable/datatable.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { DatatableSortKeyDirective } from "./sort-key/sort-key.directive";
 import { DatatableDefaultsDirective } from "./defaults/defaults.directive";
 import { DatatablePaginationDirective } from "./pagination/pagination.directive";
-import { VirtualDatatablePaginationDirective } from "./virtualDatatablePagination/virtual-datatable-pagination.directive";
+import { VirtualDatatablePaginationDirective } from "./virtual-datatable-pagination/virtual-datatable-pagination.directive";
 
 const directives = [
   DatatableDefaultsDirective,
