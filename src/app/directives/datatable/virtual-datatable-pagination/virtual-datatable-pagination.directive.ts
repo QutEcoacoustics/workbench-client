167d98797cd66bb9175c7e15ccba740c9d3f335d
hudson-newey <grath@duck.com>
2025-02-14 11:19:14 +1000

    Finish virtual pagination tests

--- src/app/directives/datatable/virtual-datatable-pagination/virtual-datatable-pagination.directive.ts
+++ src/app/directives/datatable/virtual-datatable-pagination/virtual-datatable-pagination.directive.ts
@@ -2,7 +2,10 @@ import { Directive, Input, OnChanges } from "@angular/core";
 import { Filters } from "@baw-api/baw-api.service";
 import { DatatablePaginationDirective } from "@directives/datatable/pagination/pagination.directive";
 import { AbstractModel } from "@models/AbstractModel";
-import { map, Observable, startWith } from "rxjs";
+import { map, Observable, of, startWith } from "rxjs";
+
+export type VirtualDatabaseModelInput<T extends AbstractModel> =
+  () => Observable<T[]>;
 
 /**
  * @description
@@ -27,15 +30,21 @@ export class VirtualDatatablePaginationDirective<Model extends AbstractModel>
   extends DatatablePaginationDirective<Model>
   implements OnChanges
 {
-  @Input("bawVirtualDatatablePagination") public models: () => Observable<
-    Model[]
-  >;
+  @Input("bawVirtualDatatablePagination") public models: VirtualDatabaseModelInput<Model>;
 
   public ngOnChanges(): void {
     this.pagination = { getModels: this.getModels.bind(this) };
   }
 
   private getModels(filters: Filters<Model>): Observable<Model[]> {
+    // because the models input is optional, we want to return an empty table
+    // if no models are provided (undefined).
+    // this can occur if the models are still being fetched as an associated
+    // model.
+    if (!this.models) {
+      return of([]);
+    }
+
     const pageNumber = filters.paging.page ?? 1;
     const pageSize = this.rowLimit;
 
