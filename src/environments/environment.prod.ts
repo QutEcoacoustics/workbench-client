a56a7a7ad94c697f9155e90bc7ec21a908d13a0b
hudson-newey <neweyh@qut.edu.au>
2025-03-18 16:20:03 +1000

    Increase prod timeout to match dev timeout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   testing: false,
   production: true,
   ssrTimeout: 100,
-  browserTimeout: 10_000,
+  browserTimeout: 20_000,
   // Version is set by the docker container, edit with care
   version: "<<VERSION_REPLACED_WHEN_BUILT>>",
 };
