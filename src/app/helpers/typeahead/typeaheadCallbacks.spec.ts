18b8d133b6f9a90e039ea81080407c274a248ffc
hudson-newey <neweyh@qut.edu.au>
2025-04-03 16:38:17 +1000

    Add typeahead callback helper tests
    [skip ci]

--- src/app/helpers/typeahead/typeaheadCallbacks.spec.ts
+++ src/app/helpers/typeahead/typeaheadCallbacks.spec.ts
@@ -1,4 +1,5 @@
 import { firstValueFrom } from "rxjs";
+import { modelData } from "@test/helpers/faker";
 import { createItemSearchCallback } from "./typeaheadCallbacks";
 
 describe("typeaheadCallbacks", () => {
@@ -15,10 +16,41 @@ describe("typeaheadCallbacks", () => {
       expect(realizedResults).toEqual(expectedResults);
     });
 
-    it("should limit the number of results to maxResults", () => {});
+    it("should limit the number of results to maxResults", async () => {
+      const expectedMaxResults = 10;
+      // we have to multiply the expected max results by at least 2 to ensure
+      // that the tested length is greater than the expected max results
+      const testedLength =
+        expectedMaxResults * modelData.datatype.number({ min: 2, max: 20 });
 
-    it("should return an empty array if no items match the search term", () => {});
+      const callback = createItemSearchCallback(
+        Array.from({ length: testedLength }, (_, i) => `Item ${i}`)
+      );
 
-    it("should return an empty array if no search term is provided", () => {});
+      const searchTerm = "Item";
+      const results = await firstValueFrom(callback(searchTerm, []));
+
+      expect(results.length).toEqual(expectedMaxResults);
+    });
+
+    it("should return an empty array if no items match the search term", async () => {
+      const callback = createItemSearchCallback(["apple", "banana", "cherry"]);
+      const searchTerm = "orange";
+
+      const expectedResults: string[] = [];
+      const realizedResults = await firstValueFrom(callback(searchTerm, []));
+
+      expect(realizedResults).toEqual(expectedResults);
+    });
+
+    it("should return all items if no search term is provided", async () => {
+      const testedItems = ["apple", "banana", "cherry"];
+      const callback = createItemSearchCallback(testedItems);
+
+      const searchTerm = "";
+      const realizedResults = await firstValueFrom(callback(searchTerm, []));
+
+      expect(realizedResults).toEqual(testedItems);
+    });
   });
 });
