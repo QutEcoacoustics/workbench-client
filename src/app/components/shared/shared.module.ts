105cbacebbd0add84fd164e8d322d00c7323b5be
hudson-newey <hudsonnewey@gmail.com>
2025-04-14 15:03:53 +1000

    Greatly reduce shared module
    I think I can safely remove this module. However, I have to tripple
    check

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from "@angular/core";
-import {
-  internalComponents,
-  internalModules,
-  sharedComponents,
-  sharedModules,
-} from "./shared.components";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MenuModule } from "@menu/menu.module";
+import { FormlyBootstrapModule } from "@ngx-formly/bootstrap";
+import { FormlyModule } from "@ngx-formly/core";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+import { DateValueAccessorModule } from "angular-date-value-accessor";
+
+const sharedModules = [
+  BrowserAnimationsModule,
+  DateValueAccessorModule,
+
+  FormlyModule,
+  FormsModule,
+  ReactiveFormsModule,
+  FormlyBootstrapModule,
+
+  NgxDatatableModule,
+
+  MenuModule,
+];
+
 
 /**
  * Shared Components Module
  */
 @NgModule({
-  imports: [...internalModules, ...internalComponents, ...sharedComponents],
-  exports: [...sharedModules, ...sharedComponents],
+  exports: [...sharedModules],
 })
 export class SharedModule {}
