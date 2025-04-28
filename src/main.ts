e5e05621fec87d8d3cb4ca76997688e260915734
hudson-newey <hudsonnewey@gmail.com>
2025-04-28 16:12:40 +1000

    Revert eager bootstrapping
    I don't fully understand why we wait for the DOMContentLoaded event to
    bootstrap Angular. Especially since it is not the recommended behavior
    by the Angular team.
    
    I want to fully understand why we delay bootstrapping to ensure that I
    don't accidently introduce an undocumented bug that previously existed.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,21 @@ if (environment.production) {
   enableProdMode();
 }
 
+// TODO: we should find out if we still need to await the page load and why we
+// originally added this code.
+// Angular doesn't recommend this, so I suspect that it was added to fix some
+// sort of undocumented bug.
+// Await page load
+const domContentLoadedPromise = new Promise<void>((resolve) =>
+  document.addEventListener("DOMContentLoaded", () => {
+    document.removeEventListener("DOMContentLoader", () => {});
+    resolve();
+  })
+);
+
 // Bootstrap Angular
-bootstrapApplication(AppComponent, appConfig).catch((err) => {
-  console.error(err);
+domContentLoadedPromise.then(() => {
+  bootstrapApplication(AppComponent, appConfig).catch((err) => {
+    console.error(err);
+  });
 });
