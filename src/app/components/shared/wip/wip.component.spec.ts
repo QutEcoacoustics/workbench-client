f646007d85fb807404f6969ffc2694aa9fafb509
Hudson Newey <neweyh@qut.edu.au>
2025-06-30 00:15:51 +0000

    Hide WIP for non-admins (#2344)
    Fixes: #2335

--- src/app/components/shared/wip/wip.component.spec.ts
+++ src/app/components/shared/wip/wip.component.spec.ts
@@ -2,10 +2,14 @@ import { BawSessionService } from "@baw-api/baw-session.service";
 import { createComponentFactory, Spectator } from "@ngneat/spectator";
 import { provideMockConfig } from "@services/config/provide-configMock";
 import { IconsModule } from "@shared/icons/icons.module";
+import { User } from "@models/User";
+import { generateUser } from "@test/fakes/User";
 import { WIPComponent } from "./wip.component";
 
 describe("WIPComponent", () => {
   let spec: Spectator<WIPComponent>;
+
+  const wipContent = () => spec.query(".wip-content");
 
   const createComponent = createComponentFactory({
     component: WIPComponent,
@@ -13,11 +17,40 @@ describe("WIPComponent", () => {
     providers: [BawSessionService, provideMockConfig()],
   });
 
+  function setup(userModel?: User) {
+    spec = createComponent({ detectChanges: false});
+
+    const sessionService = spec.inject(BawSessionService);
+    spyOnProperty(sessionService, "loggedInUser").and.returnValue(userModel);
+
+    spec.detectChanges();
+  }
+
   beforeEach(() => {
     spec = createComponent();
   });
 
   it("should create", () => {
-    expect(spec.component).toBeTruthy();
+    setup();
+    expect(spec.component).toBeInstanceOf(WIPComponent);
   });
+
+  it("should be hidden for guest users", () => {
+    setup();
+    expect(wipContent()).not.toExist();
+  });
+
+  it("should be hidden for non-admin logged in users", () => {
+    const user = new User(generateUser({}, false));
+    setup(user);
+
+    expect(wipContent()).not.toExist();
+  });
+
+  it("should be visible for admin users", () => {
+    const user = new User(generateUser({}, true));
+    setup(user);
+
+    expect(wipContent()).toExist();
+  })
 });
