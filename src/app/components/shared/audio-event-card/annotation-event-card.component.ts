888a1367289f9fc130916b1d69a05934f457af37
hudson-newey <hudsonnewey@gmail.com>
2025-07-14 15:10:02 +1000

    Improve performance of annotation-event-card's

--- src/app/components/shared/audio-event-card/annotation-event-card.component.ts
+++ src/app/components/shared/audio-event-card/annotation-event-card.component.ts
@@ -4,12 +4,14 @@ import {
   CUSTOM_ELEMENTS_SCHEMA,
   ElementRef,
   input,
-  OnInit,
   viewChild,
 } from "@angular/core";
 import { MediaControlsComponent } from "@ecoacoustics/web-components/@types/components/media-controls/media-controls";
 import { Annotation } from "@models/data/Annotation";
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { SpectrogramComponent } from "@ecoacoustics/web-components/@types";
+import { LoadingComponent } from "@shared/loading/loading.component";
+import { InlineListComponent } from "@shared/inline-list/inline-list.component";
 import { ZonedDateTimeComponent } from "../datetime-formats/datetime/zoned-datetime/zoned-datetime.component";
 import { IsUnresolvedPipe } from "../../../pipes/is-unresolved/is-unresolved.pipe";
 
@@ -17,25 +19,28 @@ import { IsUnresolvedPipe } from "../../../pipes/is-unresolved/is-unresolved.pip
   selector: "baw-annotation-event-card",
   templateUrl: "./annotation-event-card.component.html",
   styleUrl: "./annotation-event-card.component.scss",
-  imports: [FaIconComponent, ZonedDateTimeComponent, IsUnresolvedPipe],
+  imports: [
+    FaIconComponent,
+    ZonedDateTimeComponent,
+    IsUnresolvedPipe,
+    LoadingComponent,
+    InlineListComponent
+],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AnnotationEventCardComponent implements OnInit, AfterViewInit {
+export class AnnotationEventCardComponent implements AfterViewInit {
   public annotation = input.required<Annotation>();
-
-  protected spectrogramId: string;
 
   // Note that there is no { static: true } option for viewChild signals.
   // This is on purpose, because signals are supposed to be able to
   // automatically detect non-changes.
   // https://github.com/angular/angular/issues/54376
-  private mediaControls = viewChild<ElementRef<MediaControlsComponent>>("mediaControls");
+  private mediaControls =
+    viewChild<ElementRef<MediaControlsComponent>>("mediaControls");
+  private spectrogram =
+    viewChild<ElementRef<SpectrogramComponent>>("spectrogram");
 
-  public ngOnInit(): void {
-    this.spectrogramId = `spectrogram-${this.annotation().id}`;
-  }
-
-  public ngAfterViewInit(): void {
-    this.mediaControls().nativeElement.for = this.spectrogramId;
+  public ngAfterViewInit() {
+    this.mediaControls().nativeElement.for = this.spectrogram().nativeElement;
   }
 }
