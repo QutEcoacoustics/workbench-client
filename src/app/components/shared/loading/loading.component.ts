8daba928b5359a911c26f41124639ab12b0c220a
hudson-newey <neweyh@qut.edu.au>
2025-10-09 09:25:29 +1000

    Fix layout reflow in loading spinner
    - Fixes reflow in loading.component
    - Migrates loading.component to signals
    
    Fixes: #2457

--- src/app/components/shared/loading/loading.component.ts
+++ src/app/components/shared/loading/loading.component.ts
@@ -1,36 +1,32 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from "@angular/core";
 import {
   BootstrapColorTypes,
   BootstrapScreenSizes,
 } from "@helpers/bootstrapTypes";
-import { NgClass } from "@angular/common";
 
 /**
  * Loading Animation
  */
 @Component({
   selector: "baw-loading",
-  template: `
-    <div class="d-flex justify-content-center m-0 p-0">
-      <div id="spinner" role="status" [ngClass]="spinnerClass">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-    </div>
-  `,
-  imports: [NgClass],
+  templateUrl: "./loading.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoadingComponent implements OnInit {
-  @Input() public color: BootstrapColorTypes = "info";
-  @Input() public size: BootstrapScreenSizes = "md";
-  @Input() public type: "border" | "grower" = "border";
+export class LoadingComponent {
+  public readonly color = input<BootstrapColorTypes>("info");
+  public readonly size = input<BootstrapScreenSizes>("md");
+  public readonly type = input<"border" | "grower">("border");
 
-  public spinnerClass: { [klass: string]: true };
-
-  public ngOnInit(): void {
-    this.spinnerClass = {
-      [`spinner-${this.type}`]: true,
-      [`spinner-${this.type}-${this.size}`]: true,
-      [`text-${this.color}`]: true,
+  protected readonly spinnerClass = computed(() => {
+    return {
+      [`spinner-${this.type()}`]: true,
+      [`spinner-${this.type()}-${this.size()}`]: true,
+      [`text-${this.color()}`]: true,
     };
-  }
+  });
 }
