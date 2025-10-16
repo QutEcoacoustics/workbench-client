103937e16f2bfb46ccad85b3e9e803899122300f
hudson-newey <neweyh@qut.edu.au>
2025-10-16 15:50:32 +1000

    Event modal tests

--- src/app/components/shared/event-modal/event-modal.component.spec.ts
+++ src/app/components/shared/event-modal/event-modal.component.spec.ts
@@ -1,29 +1,40 @@
-import { createComponent } from "@angular/core";
 import { createComponentFactory, mockProvider, Spectator } from "@ngneat/spectator";
 import { EventModalComponent } from "./event-modal.component";
 import { provideMockBawApi } from "@baw-api/provide-baw-ApiMock";
 import { AnnotationService } from "@services/models/annotations/annotation.service";
 import { Annotation } from "@models/data/Annotation";
 import { generateAnnotation } from "@test/fakes/data/Annotation";
+import { AnnotationEventCardComponent } from "@shared/audio-event-card/annotation-event-card.component";
 
 describe("EventModalComponent", () => {
   let spec: Spectator<EventModalComponent>;
+  let annotation: Annotation;
 
   const createComponent = createComponentFactory({
     component: EventModalComponent,
     providers: [
       provideMockBawApi(),
       mockProvider(AnnotationService, {
-        show: async () => new Annotation(generateAnnotation()),
+        show: () => Promise.resolve(annotation),
       }),
     ],
   });
 
   beforeEach(() => {
+    annotation = new Annotation(generateAnnotation());
+
     spec = createComponent();
   });
 
   it("should create", () => {
     expect(spec.component).toBeInstanceOf(EventModalComponent);
+  });
+
+  // TODO: The async pipe does not seem to resolve in the test environment
+  xit("should correctly attach the annotation to the annotation event card", () => {
+    const eventCard = spec.query(AnnotationEventCardComponent);
+    expect(eventCard).toBeVisible();
+
+    expect(eventCard.annotation).toEqual(annotation);
   });
 });
