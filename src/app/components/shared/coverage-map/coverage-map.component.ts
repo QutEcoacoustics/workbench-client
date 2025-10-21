0133711b4f79e850def1375de791dac20d75f604
hudson-newey <neweyh@qut.edu.au>
2025-10-21 13:26:05 +1000

    Refactor coverage map into its own component

---
+++ src/app/components/shared/coverage-map/coverage-map.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "baw-coverage-map",
+  templateUrl: "./coverage-map.component.html",
+})
+export class CoverageMapComponent {
+}
