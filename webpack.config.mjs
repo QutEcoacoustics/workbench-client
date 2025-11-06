710c3398e17b44f551c7c05c53d39c75808024f8
hudson-newey <neweyh@qut.edu.au>
2025-11-06 17:02:32 +1000

    Static web components build with angular linker
    [skip ci]

---
+++ webpack.config.mjs
@@ -0,0 +1,18 @@
+import linkerPlugin from '@angular/compiler-cli/linker/babel';
+export default {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [linkerPlugin],
+            compact: false,
+            cacheDirectory: true,
+          }
+        }
+      }
+    ]
+  }
+}
