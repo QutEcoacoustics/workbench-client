bacdd2b8c08430aa83f6ccac6aa89e81f2217182
hudson-newey <neweyh@qut.edu.au>
2025-11-07 17:02:51 +1000

    Semi-working library providers

--- projects/web-components/src/public-api.ts
+++ projects/web-components/src/public-api.ts
@@ -1,10 +1,28 @@
 import { createCustomElement } from "@angular/elements";
 import { createApplication } from "@angular/platform-browser";
 import { EventMapWebComponent } from "./lib/components/event-map/event-map.web.component";
+import { APP_ID } from "@angular/core";
+import { GoogleMap, MapAdvancedMarker, MapInfoWindow, MapMarkerClusterer } from "@angular/google-maps";
 
 (async () => {
   const app = await createApplication({
-    providers: [],
+    providers: [
+      // event-map.component dependencies
+      // MapComponent,
+
+      // map.component dependencies
+      GoogleMap,
+      MapAdvancedMarker,
+      MapMarkerClusterer,
+      MapInfoWindow,
+      // LoadingComponent,
+      // NgTemplateOutlet,
+
+      // provideConfig(),
+      // provideBawApi(),
+
+      { provide: APP_ID, useValue: "workbench-client" },
+    ],
   });
 
   const eventMapWebComponent = createCustomElement(EventMapWebComponent, {
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,18 +1,19 @@
-import linkerPlugin from '@angular/compiler-cli/linker/babel';
+import linkerPlugin from "@angular/compiler-cli/linker/babel";
+
 export default {
   module: {
     rules: [
       {
         test: /\.m?js$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             plugins: [linkerPlugin],
             compact: false,
             cacheDirectory: true,
-          }
-        }
-      }
-    ]
-  }
-}
+          },
+        },
+      },
+    ],
+  },
+};
