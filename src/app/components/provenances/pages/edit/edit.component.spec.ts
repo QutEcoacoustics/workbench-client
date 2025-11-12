11e737eebac74939ba05ba4e677a72baab6d3c30
hudson-newey <hudsonnewey@gmail.com>
2025-11-12 16:41:55 +1000

    Provenance edit tests

--- src/app/components/provenances/pages/edit/edit.component.spec.ts
+++ src/app/components/provenances/pages/edit/edit.component.spec.ts
@@ -1,89 +1,127 @@
-// import { provideMockBawApi } from "@baw-api/provide-baw-ApiMock";
-// import {
-//   provenanceResolvers,
-//   ProvenanceService,
-// } from "@baw-api/provenance/provenance.service";
-// import {
-//   IProvenance,
-//   Provenance,
-// } from "@models/Provenance";
-// import {
-//   createRoutingFactory,
-//   SpectatorRouting,
-//   SpyObject,
-// } from "@ngneat/spectator";
-// import { FormComponent } from "@shared/form/form.component";
-// import { generateProvenance } from "@test/fakes/Provenance";
-// import { assertPageInfo } from "@test/helpers/pageRoute";
-// import { ToastService } from "@services/toasts/toasts.service";
-// import { of } from "rxjs";
-// import schema from "../../provenance.schema.json";
-// import { ProvenanceEditComponent } from "./edit.component";
+import {
+  createRoutingFactory,
+  SpectatorRouting,
+  SpyObject,
+} from "@ngneat/spectator";
+import { FormComponent } from "@shared/form/form.component";
+import { testFormImports, testFormProviders } from "@test/helpers/testbed";
+import { ToastService } from "@services/toasts/toasts.service";
+import { assertPageInfo } from "@test/helpers/pageRoute";
+import { generateProvenance } from "@test/fakes/Provenance";
+import { Provenance } from "@models/Provenance";
+import {
+  provenanceResolvers,
+  ProvenanceService,
+} from "@baw-api/provenance/provenance.service";
+import { of } from "rxjs";
+import { testFormlyFields } from "@test/helpers/formly";
+import schema from "../../provenance.base.schema.json";
+import { EditProvenanceComponent } from "./edit.component";
 
-// describe("ProvenanceEditComponent", () => {
-//   let provenanceApi: SpyObject<ProvenanceService>;
-//   let defaultProvenance: Provenance;
-//   let spectator: SpectatorRouting<ProvenanceEditComponent>;
+describe("EditProvenanceComponent", () => {
+  const { fields } = schema;
 
-//   const createComponent = createRoutingFactory({
-//     component: ProvenanceEditComponent,
-//     imports: [FormComponent],
-//     providers: [provideMockBawApi()],
-//     mocks: [ToastService],
-//   });
+  let spec: SpectatorRouting<EditProvenanceComponent>;
+  let apiSpy: SpyObject<ProvenanceService>;
+  let mockModel: Provenance;
 
-//   assertPageInfo<Provenance>(ProvenanceEditComponent, "Edit", {
-//     provenance: {
-//       model: new Provenance(generateProvenance()),
-//     },
-//   });
+  const createComponent = createRoutingFactory({
+    component: EditProvenanceComponent,
+    imports: [...testFormImports, FormComponent],
+    providers: testFormProviders,
+    mocks: [ToastService],
+  });
 
-//   function setup(provenance: Provenance) {
-//     spectator = createComponent({
-//       detectChanges: false,
-//       data: {
-//         resolvers: {
-//           provenance: provenanceResolvers.show,
-//         },
-//         provenance: { model: provenance },
-//       },
-//     });
-//     provenanceApi = spectator.inject(ProvenanceService);
-//     spectator.detectChanges();
-//   }
+  beforeEach(() => {
+    mockModel = new Provenance(generateProvenance());
 
-//   beforeEach(() => {
-//     defaultProvenance = new Provenance(
-//       generateProvenance({ name: "Test Provenance" })
-//     );
-//   });
+    spec = createComponent({
+      detectChanges: false,
+      params: { provenanceId: mockModel.id },
+      data: {
+        resolvers: {
+          provenance: provenanceResolvers.show,
+        },
+        provenance: { model: mockModel },
+      },
+    });
 
-//   it("should create", () => {
-//     setup(defaultProvenance);
-//     expect(spectator.component).toBeTruthy();
-//   });
+    apiSpy = spec.inject(ProvenanceService);
+    apiSpy.update.and.returnValue(of());
 
-//   it("should have title", () => {
-//     setup(defaultProvenance);
-//     expect(spectator.component.title).toBe("Edit Test Provenance");
-//   });
+    spec.detectChanges();
+  });
 
-//   it("should have fields", () => {
-//     setup(defaultProvenance);
-//     expect(spectator.component.fields.length).toBe(schema.fields.length);
-//   });
+  assertPageInfo(EditProvenanceComponent, "Edit");
 
-//   it("should call update on submission", () => {
-//     setup(defaultProvenance);
-//     provenanceApi.update.and.returnValue(of(defaultProvenance));
+  it("should create", () => {
+    expect(spec.component).toBeInstanceOf(EditProvenanceComponent);
+  });
 
-//     const data: Partial<IProvenance> = {
-//       name: "Updated Provenance",
-//     };
-//     spectator.component.submit(data);
+  it("should call the api with the correct model when the form is submitted", () => {
+    const updatedModel = new Provenance(generateProvenance());
+    spec.component.submit(updatedModel);
+    expect(apiSpy.update).toHaveBeenCalledWith(updatedModel);
+  });
 
-//     expect(provenanceApi.update).toHaveBeenCalledWith(
-//       jasmine.objectContaining({ name: "Updated Provenance" })
-//     );
-//   });
-// });
+  it("should redirect correctly after submission", () => {
+    const updatedModel = new Provenance(generateProvenance());
+    apiSpy.update.and.returnValue(of(updatedModel));
+
+    expect(apiSpy.update).not.toHaveBeenCalled();
+
+    spec.component.submit(mockModel);
+
+    expect(spec.router.navigateByUrl).toHaveBeenCalledWith(
+      `/provenances/${updatedModel.id}`,
+    );
+  });
+
+  describe("form", () => {
+    testFormlyFields([
+      {
+        testGroup: "Provenance Name Input",
+        field: fields[0],
+        key: "name",
+        label: "Provenance Name",
+        type: "input",
+        inputType: "text",
+        required: true,
+      },
+      {
+        testGroup: "Version Input",
+        field: fields[1],
+        key: "version",
+        label: "Version",
+        type: "input",
+        inputType: "text",
+        required: true,
+      },
+      {
+        testGroup: "Description Input",
+        field: fields[2],
+        key: "description",
+        label: "Description",
+        type: "textarea",
+      },
+      {
+        testGroup: "Score Minimum Input",
+        field: fields[3],
+        key: "scoreMinimum",
+        label: "Score Minimum",
+        type: "input",
+        inputType: "number",
+        required: false,
+      },
+      {
+        testGroup: "Score Maximum Input",
+        field: fields[4],
+        key: "scoreMaximum",
+        label: "Score Maximum",
+        type: "input",
+        inputType: "number",
+        required: false,
+      },
+    ]);
+  });
+});
