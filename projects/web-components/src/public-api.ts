c5bdb93e677aba8dc1fae56d1e193de6a314d467
hudson-newey <hudsonnewey@gmail.com>
2025-12-22 11:49:22 +1000

    Provide full app DI to web components

--- projects/web-components/src/public-api.ts
+++ projects/web-components/src/public-api.ts
@@ -1,8 +1,12 @@
+import { APP_ID } from "@angular/core";
 import { createCustomElement } from "@angular/elements";
+import { GoogleMap, MapAdvancedMarker, MapInfoWindow, MapMarkerClusterer } from "@angular/google-maps";
 import { createApplication } from "@angular/platform-browser";
 import { EventMapWebComponent } from "./lib/components/event-map/event-map.web.component";
-import { APP_ID } from "@angular/core";
-import { GoogleMap, MapAdvancedMarker, MapInfoWindow, MapMarkerClusterer } from "@angular/google-maps";
+
+const webComponentMappings = new Map<string, any>([
+  ["oe-baw-event-map", EventMapWebComponent],
+]);
 
 (async () => {
   const app = await createApplication({
@@ -25,9 +29,8 @@ import { GoogleMap, MapAdvancedMarker, MapInfoWindow, MapMarkerClusterer } from 
     ],
   });
 
-  const eventMapWebComponent = createCustomElement(EventMapWebComponent, {
-    injector: app.injector,
-  });
-
-  customElements.define("oe-baw-event-map", eventMapWebComponent);
+  for (const [selector, component] of webComponentMappings) {
+    const customElementComponent = createCustomElement(component, app);
+    customElements.define(selector, customElementComponent);
+  }
 })();
