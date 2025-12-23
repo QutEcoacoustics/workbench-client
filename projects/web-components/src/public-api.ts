5f979e43ad8370145ef52e6fc715a4a7cbe2e6e1
hudson-newey <neweyh@qut.edu.au>
2025-12-23 12:20:14 +1000

    Explicit no exports from public-api.ts
    [skip ci]

--- projects/web-components/src/public-api.ts
+++ projects/web-components/src/public-api.ts
@@ -1,57 +1,13 @@
-import { APP_ID } from "@angular/core";
-import { createCustomElement } from "@angular/elements";
-import { createApplication } from "@angular/platform-browser";
-import { provideBawApi } from "@baw-api/provide-baw-api";
-import { Configuration, IConfiguration } from "@helpers/app-initializer/app-initializer";
-import { API_CONFIG } from "@services/config/config.tokens";
-import { provideConfig } from "@services/config/provide-config";
-import { IS_WEB_COMPONENT_TARGET } from "src/app/app.helper";
-import { applyMonkeyPatches } from "src/patches/patches";
-import { defaultConfig } from "./defaultConfig";
+import { IConfiguration } from "@helpers/app-initializer/app-initializer";
 import { EventMapWebComponent } from "./lib/components/event-map/event-map.web.component";
+import { registerWebComponents } from "./register";
 
 const bawConfigName = "__baw_config__";
 const webComponentMappings = new Map<string, any>([
   ["oe-event-map", EventMapWebComponent],
 ]);
 
-// We bootstrap the application using an async IIFE because top-level await is
-// not supported in all browsers/build environments yet.
-(async () => {
-  applyMonkeyPatches();
-
-  const app = await createApplication({
-    providers: [
-      provideConfig(),
-      provideBawApi(),
-
-      { provide: APP_ID, useValue: "workbench-client" },
-      { provide: API_CONFIG, useFactory: configFactory },
-      { provide: IS_WEB_COMPONENT_TARGET, useValue: true },
-    ],
-  });
-
-  for (const [selector, component] of webComponentMappings) {
-    const customElementComponent = createCustomElement(component, app);
-    customElements.define(selector, customElementComponent);
-  }
-})();
-
-/**
- * @description
- * A DI provider factory that returns the application {@linkcode Configuration}
- * object.
- * This factory differs from the standard workbench provider as it allows for
- * `window`-level overrides.
- */
-function configFactory(): Configuration {
-  const windowLevelConfig = window[bawConfigName];
-  if (windowLevelConfig) {
-    return new Configuration(windowLevelConfig);
-  }
-
-  return new Configuration(defaultConfig);
-}
+registerWebComponents(webComponentMappings, bawConfigName);
 
 declare global {
   interface Window {
---
+++ projects/web-components/src/register.ts
@@ -0,0 +1,51 @@
+import { APP_ID } from "@angular/core";
+import { createCustomElement } from "@angular/elements";
+import { createApplication } from "@angular/platform-browser";
+import { provideBawApi } from "@baw-api/provide-baw-api";
+import { Configuration } from "@helpers/app-initializer/app-initializer";
+import { API_CONFIG } from "@services/config/config.tokens";
+import { provideConfig } from "@services/config/provide-config";
+import { IS_WEB_COMPONENT_TARGET } from "src/app/app.helper";
+import { applyMonkeyPatches } from "src/patches/patches";
+import { defaultConfig } from "./defaultConfig";
+
+export async function registerWebComponents(
+  mappings: Map<string, any>,
+  configNamespace: string,
+) {
+  applyMonkeyPatches();
+
+  const app = await createApplication({
+    providers: [
+      provideConfig(),
+      provideBawApi(),
+
+      { provide: APP_ID, useValue: "workbench-client" },
+      { provide: API_CONFIG, useFactory: configFactory(configNamespace) },
+      { provide: IS_WEB_COMPONENT_TARGET, useValue: true },
+    ],
+  });
+
+  for (const [selector, component] of mappings) {
+    const customElementComponent = createCustomElement(component, app);
+    customElements.define(selector, customElementComponent);
+  }
+}
+
+/**
+ * @description
+ * A DI provider factory that returns the application {@linkcode Configuration}
+ * object.
+ * This factory differs from the standard workbench provider as it allows for
+ * `window`-level overrides.
+ */
+function configFactory(configNamespace: string): () => Configuration {
+  return () => {
+    const windowLevelConfig = window[configNamespace];
+    if (windowLevelConfig) {
+      return new Configuration(windowLevelConfig);
+    }
+
+    return new Configuration(defaultConfig);
+  };
+}
