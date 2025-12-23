f3f35fc82cefb2c73637d33a14150f9546fdc973
hudson-newey <neweyh@qut.edu.au>
2025-12-23 13:47:22 +1000

    Remove ng-package and explicit publicApi
    [skip ci]

--- projects/web-components/src/publicApi.ts
+++ projects/web-components/src/publicApi.ts
@@ -2,6 +2,12 @@ import { IConfiguration } from "@helpers/app-initializer/app-initializer";
 import { EventMapWebComponent } from "./lib/components/event-map/event-map.web.component";
 import { registerWebComponents } from "./register";
 
+// Anything exported from this file is intended to be used as a public API.
+//
+// Note at the moment, there is nothing exported from this file.
+// Therefore the file is intended to be used as a module only for its
+// custom-element and typing side-effects.
+
 const bawConfigName = "__baw_config__";
 const webComponentMappings = new Map<string, any>([
   ["oe-event-map", EventMapWebComponent],
@@ -9,6 +15,9 @@ const webComponentMappings = new Map<string, any>([
 
 registerWebComponents(webComponentMappings, bawConfigName);
 
+// Patch the global namespace to include the custom baw config property.
+// We do this so that TypeScript users do not get type errors when trying to
+// set the configuration at the window level.
 declare global {
   interface Window {
     /**
